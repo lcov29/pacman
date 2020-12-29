cb2b01db27565bcb65e96bc2a417f8895b30a8c6
Lucas Vossen <lucas.vossen@hotmail.de>
2020-12-29 14:54:29 +0100

    add Ghost.js

---
+++ source/Ghost.js
@@ -0,0 +1,69 @@
+"use strict";
+
+class Ghost extends MovableObject {
+	
+	
+	constructor(game, xPosition, yPosition) {
+		super(game, xPosition, yPosition);
+		this.occupied_field_object = 'empty';
+	}
+	
+	
+	moveToPosition(position) {
+		this.setNextPosition(position.xPosition, position.yPosition)
+		this.handlePacManCollision();
+		this.updateField();
+		this.updateCurrentPosition();
+	}
+	
+    
+    setNextPosition(xPosition, yPosition) {
+        this.next_xPosition = xPosition;
+        this.next_yPosition = yPosition;
+    }
+    
+
+	handlePacManCollision() {
+		if (this.isNextFieldPositionEqual('pacman')) {
+				this.decrementLifeOfPacman();
+		}
+	}
+
+	
+	updateField() {
+		this.game.field.addUpdateRequest(new UpdateRequest(this.xPosition, this.yPosition, this.occupied_field_object));
+		this.updateOccupiedFieldObject();
+		this.game.field.addUpdateRequest(new UpdateRequest(this.next_xPosition, this.next_yPosition, 'ghost'));
+    }
+    
+
+    updateCurrentPosition() {
+		this.xPosition = this.next_xPosition;
+		this.yPosition = this.next_yPosition;
+	}
+    
+    	
+	decrementLifeOfPacman() {
+		for (let pacman of this.game.pacmans) {
+			if (pacman.xPosition == this.next_xPosition && pacman.yPosition == this.next_yPosition) {
+				pacman.decrementLife();
+			}
+		}
+	}
+	
+	
+	//TODO: check for edge cases
+	updateOccupiedFieldObject() {
+		var next_field_object = this.game.field.getFieldObject(this.next_xPosition, this.next_yPosition);
+		switch(next_field_object) {
+			case 'ghost':
+			case 'pacman':
+				this.occupied_field_object = 'empty';
+				break;
+			default:
+				this.occupied_field_object = this.game.field.getFieldObject(this.next_xPosition, this.next_yPosition);
+				break;
+		}
+	}
+	
+}
