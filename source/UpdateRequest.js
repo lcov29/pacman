bf595cb08eadffcfd2d3cdacbcc25f202b80f039
Lucas Vossen <lucas.vossen@hotmail.de>
2020-12-30 16:31:05 +0100

    add UpdateRequest.js

---
+++ source/UpdateRequest.js
@@ -0,0 +1,11 @@
+"use strict";
+
+class UpdateRequest {
+	
+	constructor(xPosition, yPosition, object) {
+		this.xPosition = xPosition;
+		this.yPosition = yPosition;
+		this.object = object;
+	}
+	
+}
