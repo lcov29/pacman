aff6fd6c7b2f6118fcc8951094ddb5e2b02d1423
voss29 <lucas.vossen@hotmail.de>
2021-10-21 20:57:24 +0200

    add optional property direction

--- source/UpdateRequest.js
+++ source/UpdateRequest.js
@@ -2,10 +2,11 @@
 
 class UpdateRequest {
    
-   constructor(xPosition, yPosition, object) {
+   constructor(xPosition, yPosition, object, direction = "") {
       this.xPosition = xPosition;
       this.yPosition = yPosition;
       this.object = object;
+      this.direction = direction;
    }
    
 }
