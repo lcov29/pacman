005f23bc1dc7342a14ff5a1e88d570121b0a4c35
Lucas Vossen <lv_code@outlook.com>
2021-12-06 22:07:21 +0100

    add class

---
+++ source/BoardPosition.js
@@ -0,0 +1,18 @@
+"use strict";
+
+class BoardPosition {
+
+    constructor(x, y) {
+        this.x = x;
+        this.y = y;
+    }
+
+    getX() {
+        return this.x;
+    }
+
+    getY() {
+        return this.y;
+    }
+
+}
