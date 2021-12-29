ad585da3951b5f0518bc717ac7e528c8fe4a5e9a
Lucas Vossen <lv_code@outlook.com>
2021-12-29 16:14:04 +0100

    add attributes character and movement_direction

--- source/BoardPosition.js
+++ source/BoardPosition.js
@@ -2,26 +2,54 @@
 
 class BoardPosition {
 
-    constructor(x, y, id) {
+    constructor(x, y, character) {
         this.x = x;
         this.y = y;
-        this.id = id;
+        this.id = Configuration.id_unaccessible_board_element;
+        this.character = character;
+        this.movement_direction = "";
     }
+
 
     getX() {
         return this.x;
     }
 
+
     getY() {
         return this.y;
     }
+
 
     getID() {
         return this.id;
     }
 
+
+    getCharacter() {
+        return this.character;
+    }
+
+
+    getMovementDirection() {
+        return this.movement_direction;
+    }
+
+    setID(id) {
+        // prevent id of accessible elements to change after initialisation
+        this.id = (this.id == Configuration.id_unaccessible_board_element) ? id : this.id;
+    }
+
+
+    setMovementDirection(direction) {
+        this.movement_direction = direction;
+    }
+
+
     clone() {
-        return new BoardPosition(this.x, this.y, this.id);
+        var clone = new BoardPosition(this.x, this.y, this.character);
+        clone.setID(this.id);
+        return clone;
     }
 
 }
