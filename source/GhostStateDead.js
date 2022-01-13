7f4ef4f51d48028f7f4c2660b9dacecec1d71275
Lucas Vossen <lv_code@outlook.com>
2022-01-13 22:23:56 +0100

    add file

---
+++ source/GhostStateDead.js
@@ -0,0 +1,27 @@
+"use strict";
+
+
+class GhostStateDead extends GhostState {
+    constructor(ghost) {
+        super(Configuration.ghost_state_dead_name,
+              Infinity,
+              ghost,
+              Configuration.ghost_dead_foreground_css_class);
+    }
+
+
+    executeStateMovementPattern() {
+        let ghost = super.getGhost();
+        let current_position_id = ghost.getCurrentPosition().getID();
+        let spawn_position_id = ghost.getSpawnID();
+        let next_position = ghost.calculateNextRoutingPosition(current_position_id, spawn_position_id);
+        return ghost.moveToPosition(next_position.getX(), next_position.getY());
+    }
+
+
+    /*
+    getSubsequentState() {
+        return new GhostStateScatter(7, this.getGhost());
+    }*/
+
+}
