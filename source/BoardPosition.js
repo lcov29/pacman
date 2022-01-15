1033a2632a1fd1b076c70a405d2cf284235cdf29
Lucas Vossen <lv_code@outlook.com>
2022-01-15 04:33:36 +0100

    add header block

--- source/BoardPosition.js
+++ source/BoardPosition.js
@@ -1,5 +1,23 @@
 "use strict";
 
+/*  
+    =================================================================================================================
+    Representation of a tile on the board.
+
+    Note 1:     Positions that can be accessed by actors get indexed by the BoardParser (zero based)
+
+    Note 2:     Each tile is divided into two layers:
+                - actor layer (contains characters of pacman and ghosts, else character representing an empty tile)
+                - element layer (contains characters of non-actors like points, walls, powerups ...)
+
+                This separation facilitates the implementation of actor movement at the cost of a higher complexity
+                of parsing the user defined level. Actors accessing a tile do not overwrite its element and therefore
+                do not need to save and restore the element upon leaving the tile (e.g a ghost entering a tile does 
+                not consume the occupying point character)
+    =================================================================================================================
+ */
+
+
 class BoardPosition {
 
 
