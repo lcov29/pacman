9c2d8da6b05c0e9fb367cbaad0bd9dbb09b5ceff
Lucas Vossen <lv_code@outlook.com>
2022-01-16 06:08:06 +0100

    rearrange methods

--- source/Level.js
+++ source/Level.js
@@ -42,111 +42,6 @@ class Level {
     }
 
 
-    setNextPacmanDirection(direction_name) {
-        for (let pacman of this.pacmans) {
-           pacman.setMovementDirectionName(direction_name);
-        }
-    } 
-
-
-    getTeleportDestination(position) {
-        let destination = null;
-        for (let teleporter of this.teleporters) {
-            if (teleporter.isTeleporterFor(position)) {
-                destination = teleporter.getDestinationPositionFor(position);
-                break;
-            }
-        }
-        return destination;
-    }
-
-
-    getPacmanIDs() {
-        let ids = [];
-        for (let pacman of this.pacmans) {
-            ids.push(pacman.getCurrentPosition().getID());
-        }
-        return ids;
-    }
-
-
-    getTurnCompletionStatusForActor(actor_character, actor_id) {
-        let result = true;
-        let actors = (actor_character === Configuration.pacman_character) ? this.pacmans : this.ghosts;
-        for (let actor of actors) {
-            if (actor.getCurrentPosition().getID() === actor_id) {
-                result = actor.getTurnMovementStatus();
-                break;
-            }
-        }
-        return result;
-    }
-
-
-    getBoardPositionAt(x, y) {
-        return this.board.getPosition(x, y);
-    }
-
-
-    getBoardPositionArray() {
-        return this.board.buildBoardPositionArray();
-    }
-
-
-    isBoardElementTeleporter(element) {
-        return Teleporter.isElementTeleporter(element);
-    }
-
-
-    isWon() {
-        return this.available_points === 0;
-    }
-
-
-    isLost() {
-        return this.total_pacman_lifes === 0;
-    }
-
-
-    countAvailablePoints() {
-        let point_characters = [Configuration.point_character, Configuration.powerup_character];
-        return this.board.countOccurrencesOfCharacters(point_characters);
-    }
-
-
-    countInitialPacmanLifes() {
-        return this.pacmans.length * Configuration.initial_pacman_lifes;
-    } 
-
-
-    addUpdateRequest(request) {
-        this.update_requests.push(request);
-    }
-
-
-    addTeleportersToNeighborIDList(neighbor_id_list) {
-        for (let teleporter of this.teleporters) {
-            neighbor_id_list[teleporter.getIDPosition1()].push(teleporter.getIDPosition2());
-            neighbor_id_list[teleporter.getIDPosition2()].push(teleporter.getIDPosition1());
-        }
-    }
-
-
-    buildAccessibleNeighborIdList() {
-        let neighbor_id_list = this.board.buildAccessibleNeighborIdList();
-        this.addTeleportersToNeighborIDList(neighbor_id_list);
-        return neighbor_id_list;
-
-    }
-
-
-    resetTurnMovementStatusOfActors(actors) {
-        for (let actor of actors) {
-            actor.setTurnMovementStatus(false);
-        }
-    }
-
-
     executeTurn() {
         this.moveActors(this.pacmans);
         if (this.isWon() === false && this.isLost() === false) {
@@ -155,18 +50,12 @@ class Level {
     }    
 
 
-    incrementScoreBy(value) {
-        this.score += value;
-    }
-
-    
-    decrementAvailablePoints() {
-        this.available_points--;
-    }
-
-
-    decrementTotalPacmanLifes() {
-        this.total_pacman_lifes--;
+    scareLivingGhosts() {
+        for (let ghost of this.ghosts) {
+            if (ghost.getStateName() !== Configuration.ghost_state_dead_name) {
+                ghost.scare();
+            } 
+        }
     }
 
 
@@ -188,6 +77,66 @@ class Level {
     }
 
 
+    incrementScoreBy(value) {
+        this.score += value;
+    }
+
+    
+    decrementAvailablePoints() {
+        this.available_points--;
+    }
+
+
+    decrementTotalPacmanLifes() {
+        this.total_pacman_lifes--;
+    }
+
+
+    update() {
+        for (let request of this.update_requests) {
+            this.board.setPosition(request.getPosition());
+            this.game.updateView(request.getPosition(), 
+                                 request.getStyleClass(), 
+                                 this.score, 
+                                 this.total_pacman_lifes);
+        }
+        this.update_requests = [];
+    }
+
+
+    setNextPacmanDirection(direction_name) {
+        for (let pacman of this.pacmans) {
+           pacman.setMovementDirectionName(direction_name);
+        }
+    } 
+
+
+    resetTurnMovementStatusOfActors(actors) {
+        for (let actor of actors) {
+            actor.setTurnMovementStatus(false);
+        }
+    }
+
+
+    getBoardPositionAt(x, y) {
+        return this.board.getPosition(x, y);
+    }
+
+
+    getBoardPositionArray() {
+        return this.board.buildBoardPositionArray();
+    }
+
+
+    getPacmanIDs() {
+        let ids = [];
+        for (let pacman of this.pacmans) {
+            ids.push(pacman.getCurrentPosition().getID());
+        }
+        return ids;
+    }
+
+
     getStateNamesOfGhostsAt(position_id) {
         let output = [];
         let ghost_state = "";
@@ -200,6 +149,114 @@ class Level {
             }
         }
         return output;
+    }
+
+
+    getTurnCompletionStatusForActor(actor_character, actor_id) {
+        let result = true;
+        let actors = (actor_character === Configuration.pacman_character) ? this.pacmans : this.ghosts;
+        for (let actor of actors) {
+            if (actor.getCurrentPosition().getID() === actor_id) {
+                result = actor.getTurnMovementStatus();
+                break;
+            }
+        }
+        return result;
+    }
+
+
+    getTeleportDestination(position) {
+        let destination = null;
+        for (let teleporter of this.teleporters) {
+            if (teleporter.isTeleporterFor(position)) {
+                destination = teleporter.getDestinationPositionFor(position);
+                break;
+            }
+        }
+        return destination;
+    }
+
+
+    isWon() {
+        return this.available_points === 0;
+    }
+
+
+    isLost() {
+        return this.total_pacman_lifes === 0;
+    }
+   
+
+    isBoardElementTeleporter(element) {
+        return Teleporter.isElementTeleporter(element);
+    }
+
+
+    addUpdateRequest(request) {
+        this.update_requests.push(request);
+    }
+
+
+    addTeleportersToNeighborIDList(neighbor_id_list) {
+        for (let teleporter of this.teleporters) {
+            neighbor_id_list[teleporter.getIDPosition1()].push(teleporter.getIDPosition2());
+            neighbor_id_list[teleporter.getIDPosition2()].push(teleporter.getIDPosition1());
+        }
+    }
+
+
+    removeDeadPacmanAt(position_id) {
+        for (let pacman of this.pacmans) {
+            if (pacman.getCurrentPosition().getID() === position_id) {
+                this.removeElementFrom(this.pacmans, pacman);
+            }
+        }  
+    }
+
+
+    removeElementFrom(array, element) {
+        const NUMBER_OF_ELEMENTS_TO_DELETE = 1;
+        array.splice(array.indexOf(element), NUMBER_OF_ELEMENTS_TO_DELETE);
+    }
+
+
+    buildAccessibleNeighborIdList() {
+        let neighbor_id_list = this.board.buildAccessibleNeighborIdList();
+        this.addTeleportersToNeighborIDList(neighbor_id_list);
+        return neighbor_id_list;
+    }
+
+
+    buildGhostDoorDirectionMap() {
+        let ghost_door_positions = this.board.getGhostDoorPositions();
+        return GhostDoorDirectionMapper.buildMap(ghost_door_positions, this);
+    }
+
+
+    buildAccessibleNeighborList(xPosition, yPosition) {
+        return this.board.buildAccessibleNeighborList(xPosition, yPosition);
+    }
+
+
+    countAvailablePoints() {
+        let point_characters = [Configuration.point_character, Configuration.powerup_character];
+        return this.board.countOccurrencesOfCharacters(point_characters);
+    }
+
+
+    countInitialPacmanLifes() {
+        return this.pacmans.length * Configuration.initial_pacman_lifes;
+    } 
+
+
+    countScaredGhosts() {
+        let counter = 0;
+        for (let ghost of this.ghosts) {
+            if (ghost.getStateName() === Configuration.ghost_state_flee_name) {
+                counter++;
+            }
+        }
+        return counter;
     }
 
 
@@ -217,63 +274,5 @@ class Level {
         this.resetTurnMovementStatusOfActors(actors);
     }
 
-
-    scareLivingGhosts() {
-        for (let ghost of this.ghosts) {
-            if (ghost.getStateName() !== Configuration.ghost_state_dead_name) {
-                ghost.scare();
-            } 
-        }
-    }
-
-
-    countScaredGhosts() {
-        let counter = 0;
-        for (let ghost of this.ghosts) {
-            if (ghost.getStateName() === Configuration.ghost_state_flee_name) {
-                counter++;
-            }
-        }
-        return counter;
-    }
-
-
-    removeElementFrom(array, element) {
-        const NUMBER_OF_ELEMENTS_TO_DELETE = 1;
-        array.splice(array.indexOf(element), NUMBER_OF_ELEMENTS_TO_DELETE);
-    }
-
-
-    update() {
-        for (let request of this.update_requests) {
-            this.board.setPosition(request.getPosition());
-            this.game.updateView(request.getPosition(), 
-                                 request.getStyleClass(), 
-                                 this.score, 
-                                 this.total_pacman_lifes);
-        }
-        this.update_requests = [];
-    }
-
-
-    removeDeadPacmanAt(position_id) {
-        for (let pacman of this.pacmans) {
-            if (pacman.getCurrentPosition().getID() === position_id) {
-                this.removeElementFrom(this.pacmans, pacman);
-            }
-        }  
-    }
-
-
-    buildGhostDoorDirectionMap() {
-        let ghost_door_positions = this.board.getGhostDoorPositions();
-        return GhostDoorDirectionMapper.buildMap(ghost_door_positions, this);
-    }
-
-
-    buildAccessibleNeighborList(xPosition, yPosition) {
-        return this.board.buildAccessibleNeighborList(xPosition, yPosition);
-    }
-    
 
 }
