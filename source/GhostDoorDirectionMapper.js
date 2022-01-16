94b03748381dd1b81a1b97d9cd24ebdfb34f26c6
Lucas Vossen <lv_code@outlook.com>
2022-01-16 05:31:28 +0100

    add file

---
+++ source/GhostDoorDirectionMapper.js
@@ -0,0 +1,53 @@
+"use strict";
+
+/*  
+    =================================================================================================================
+    Methods for building a map containing the directions in which each ghost door should be displayed by the view.
+    Is used in Level.buildGhostDoorDirectionMap().
+    =================================================================================================================
+*/
+
+
+class GhostDoorDirectionMapper {
+
+
+    static buildMap(ghost_door_positions, level) {
+        let output = [];
+        for(let position of ghost_door_positions) {
+            let accessible_neighbors = level.buildAccessibleNeighborList(position.getX(), position.getY());
+            let ghost_door_direction = this.calculateGhostDoorDirection(accessible_neighbors);
+            output.push({id: position.getID(), direction_suffix: ghost_door_direction});
+        }
+        return output;
+    }
+
+
+    static calculateGhostDoorDirection(accessible_neighbors) {
+        let output = "";
+        switch (accessible_neighbors.length) {
+            case 0:
+            case 1:
+            case 3:
+            case 4:
+                output = Configuration.ghost_door_direction_suffix_diagonal;
+                break;
+
+            case 2:
+                let start_position = null;
+                let end_position = null;
+                for(let x = 0; x < accessible_neighbors.length; x++) {
+                    start_position = accessible_neighbors[x];
+                    for(let y = x + 1; y < accessible_neighbors.length; y++) {
+                        end_position = accessible_neighbors[y];
+                        output = Directions.calculateGhostDoorNeighborDirectionName(start_position, end_position);
+                        if(output !== "") { break; }
+                    }
+                    if (output !== "") { break; }
+                }
+                break;
+        }
+        return output;
+    }
+
+
+}
