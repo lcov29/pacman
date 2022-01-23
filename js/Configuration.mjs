dea1edc07fcdccab778409b200d2022e62606d26
Lucas Vossen <lv_code@outlook.com>
2022-01-23 14:40:50 +0100

    rearrange and convert all attributes to uppercase

--- js/Configuration.mjs
+++ js/Configuration.mjs
@@ -1,30 +1,19 @@
 "use strict";
 
 export default class Configuration {
-   
-   // TODO: CONVERT ALL ATTRIBUTE NAMES TO UPPERCASE
-   // TODO: REARRANGE ATTRIBUTES AND ADD COMMENTS
-
-
-   // Direction names
-   static direction_name_up = "up";
-   static direction_name_right = "right";
-   static direction_name_down = "down";
-   static direction_name_left = "left";
-
+ 
    
    // Game settings
-   static score_value_per_point = 10;
-   static score_value_per_powerup = 50;
-   static score_value_per_eaten_ghost = 200;
-   static initial_pacman_lifes = 1;
-   static initial_pacman_direction = Configuration.direction_name_right;
-   static initial_ghosts_direction = Configuration.direction_name_down;
+   static INITIAL_PACMAN_LIFES = 1;
+   static SCORE_VALUE_PER_POINT = 10;
+   static SCORE_VALUE_PER_POWERUP = 50;
+   static GHOST_MAX_RESPAWN_STAGE = 4;
+   static SCORE_VALUE_PER_EATEN_GHOST = 200;
+   static INTERVAL_DELAY_IN_MILLISECONDS = 500;
+   static INITIAL_PACMAN_SPRITE_DIRECTION = "right";
+   static INITIAL_GHOST_SPRITES_DIRECTION = "down";
 
-   // static initial_ghosts_direction = "down";
-   static interval_delay_in_milliseconds = 500;
-
-   static default_level = '{"board":[["#","#","#","#","#","#","#","#","#","#","#","#","#","#","#","#","#","#","#","#","#","#","#","#","#","#","#"],' + 
+   static DEFAULT_LEVEL = '{"board":[["#","#","#","#","#","#","#","#","#","#","#","#","#","#","#","#","#","#","#","#","#","#","#","#","#","#","#"],' + 
                                     '["#","o","o","o","o","o","o","o","o","o","o","o","o","#","o","o","o","o","o","o","o","o","o","o","o","o","#"],' +
                                     '["#","o","#","#","#","#","o","#","#","#","#","#","o","#","o","#","#","#","#","#","o","#","#","#","#","o","#"],' +
                                     '["#","O","#","#","#","#","o","#","#","#","#","#","o","#","o","#","#","#","#","#","o","#","#","#","#","O","#"],' +
@@ -57,108 +46,126 @@ export default class Configuration {
                                              '{"ghost":"i","x":"15","y":"10"},' +
                                              '{"ghost":"c","x":"16","y":"10"}]}';
 
-                        
-   // Internal and css representation of different game elements
-   static id_unaccessible_board_element = -1;   // must be < 0
 
-   static wall_character = "#";
-   static empty_tile_character = "x";
-   static ghost_door_character = "d";
-   static teleporter_1_tile_character = "1";
-   static teleporter_2_tile_character = "2";
-   static teleporter_3_tile_character = "3";
-   static pacman_character = "p";
 
-   static ghost_blinky_character = "b";
-   static GHOST_PINKY_CHARACTER = "y";
-   static GHOST_INKY_CHARACTER = "i";
-   static GHOST_CLYDE_CHARACTER = "c";
+   // Definition of level characters
 
-   static point_character = "o";
-   static powerup_character = "O";
+      // Actor characters
+      static PACMAN_CHARACTER = "p";
+      static GHOST_INKY_CHARACTER = "i";
+      static GHOST_PINKY_CHARACTER = "y";
+      static GHOST_CLYDE_CHARACTER = "c";
+      static GHOST_BLINKY_CHARACTER = "b";
 
-   static wall_background_css_class = "wall_tile";
-   static empty_background_css_class = "empty_tile";
-   static ghost_door_background_css_class = "ghost_door";
-   static teleporter_1_background_css_class = "teleporter_1_tile";
-   static teleporter_2_background_css_class = "teleporter_2_tile";
-   static teleporter_3_background_css_class = "teleporter_3_tile";
-
-   static pacman_foreground_css_class = "pacman";
-
-   static ghost_blinky_movement_foreground_css_class = "ghost_blinky_movement";
-   static ghost_blinky_respawn_foreground_css_class = "ghost_blinky_respawn";
-
-   static GHOST_PINKY_MOVEMENT_FOREGROUND_CSS_CLASS = "ghost_pinky_movement";
-   static GHOST_PINKY_RESPAWN_FOREGROUNG_CSS_CLASS = "ghost_pinky_respawn";
-
-   static GHOST_INKY_MOVEMENT_FOREGROUND_CSS_CLASS = "ghost_inky_movement";
-   static GHOST_INKY_RESPAWN_FOREGROUNG_CSS_CLASS = "ghost_inky_respawn";
-
-   static GHOST_CLYDE_MOVEMENT_FOREGROUND_CSS_CLASS = "ghost_clyde_movement";
-   static GHOST_CLYDE_RESPAWN_FOREGROUNG_CSS_CLASS = "ghost_clyde_respawn";
+      // Element characters
+      static WALL_CHARACTER = "#";
+      static POINT_CHARACTER = "o";
+      static POWERUP_CHARACTER = "O";
+      static EMPTY_TILE_CHARACTER = "x";
+      static GHOST_DOOR_CHARACTER = "d";
+      static TELEPORTER_1_CHARACTER= "1";
+      static TELEPORTER_2_CHARACTER = "2";
+      static TELEPORTER_3_CHARACTER = "3";
 
 
 
-   static ghost_scared_foreground_css_class = "ghost_scared";
-   static ghost_dead_foreground_css_class = "ghost_dead";
-   static point_foreground_css_class = "point";
-   static powerup_foreground_css_class = "powerup";
-   static empty_foreground_css_class = "empty_foreground";
+   // Definition of directions
 
-   static ghost_door_direction_suffix_diagonal = "crossing";
-   static ghost_door_direction_suffix_horizontal = "horizontal";
-   static ghost_door_direction_suffix_vertical = "vertical";
+      // Direction names
+      static DIRECTION_NAME_UP = "up";
+      static DIRECTION_NAME_DOWN = "down";
+      static DIRECTION_NAME_LEFT = "left";
+      static DIRECTION_NAME_RIGHT = "right";
 
-   static GHOST_STATE_CHASE_SPRITE_DISPLAY_PRIORITY = 5;
-   static GHOST_STATE_SCATTER_SPRITE_DISPLAY_PRIORITY = 4;
-   static GHOST_STATE_SCARED_SPRITE_DISPLAY_PRIORITY = 3;
-   static GHOST_STATE_RESPAWN_SPRITE_DISPLAY_PRIORITY = 2;
-   static GHOST_STATE_DEAD_SPRITE_DISPLAY_PRIORITY = 1;
+      // Ghost door 
+      static GHOST_DOOR_DIRECTION_SUFFIX_VERTICAL = "vertical";
+      static GHOST_DOOR_DIRECTION_SUFFIX_DIAGONAL = "crossing";
+      static GHOST_DOOR_DIRECTION_SUFFIX_HORIZONTAL = "horizontal";
+
+   
+   
+   // Definition of css class names
+
+      // Background
+      static WALL_BACKGROUND_CSS_CLASS = "wall_tile";
+      static EMPTY_TILE_BACKGROUND_CSS_CLASS = "empty_tile";
+      static GHOST_DOOR_BACKGROUND_CSS_CLASS = "ghost_door";
+      static TELEPORTER_1_BACKGROUND_CSS_CLASS = "teleporter_1_tile";
+      static TELEPORTER_2_BACKGROUND_CSS_CLASS = "teleporter_2_tile";
+      static TELEPORTER_3_BACKGROUND_CSS_CLASS = "teleporter_3_tile";
+
+      // Foreground
+      static EMPTY_FOREGROUND_CSS_CLASS = "empty_foreground";
+      static POINT_FOREGROUND_CSS_CLASS = "point";
+      static PACMAN_FOREGROUND_CSS_CLASS = "pacman";
+      static POWERUP_FOREGROUND_CSS_CLASS = "powerup";
+   
+      static GHOST_INKY_RESPAWN_FOREGROUNG_CSS_CLASS = "ghost_inky_respawn";
+      static GHOST_PINKY_RESPAWN_FOREGROUNG_CSS_CLASS = "ghost_pinky_respawn";
+      static GHOST_CLYDE_RESPAWN_FOREGROUNG_CSS_CLASS = "ghost_clyde_respawn";
+      static GHOST_BLINKY_RESPAWN_FOREGROUND_CSS_CLASS = "ghost_blinky_respawn";      
+
+      static GHOST_INKY_MOVEMENT_FOREGROUND_CSS_CLASS = "ghost_inky_movement";
+      static GHOST_PINKY_MOVEMENT_FOREGROUND_CSS_CLASS = "ghost_pinky_movement";
+      static GHOST_CLYDE_MOVEMENT_FOREGROUND_CSS_CLASS = "ghost_clyde_movement";
+      static GHOST_BLINKY_MOVEMENT_FOREGROUND_CSS_CLASS = "ghost_blinky_movement";
+
+      static GHOST_DEAD_FOREGROUND_CSS_CLASS = "ghost_dead";
+      static GHOST_SCARED_FOREGROUND_CSS_CLASS = "ghost_scared";
+ 
+   
+
+   // Definition of ghost sprite display priority for handling collisions between ghosts
+      static GHOST_STATE_CHASE_SPRITE_DISPLAY_PRIORITY = 5;
+      static GHOST_STATE_SCATTER_SPRITE_DISPLAY_PRIORITY = 4;
+      static GHOST_STATE_SCARED_SPRITE_DISPLAY_PRIORITY = 3;
+      static GHOST_STATE_RESPAWN_SPRITE_DISPLAY_PRIORITY = 2;
+      static GHOST_STATE_DEAD_SPRITE_DISPLAY_PRIORITY = 1;
+
+
+
+   // Definition of element lists 
+   static ACTOR_CHARACTERS = [Configuration.PACMAN_CHARACTER,
+                              Configuration.GHOST_BLINKY_CHARACTER,
+                              Configuration.GHOST_PINKY_CHARACTER,
+                              Configuration.GHOST_CLYDE_CHARACTER,
+                              Configuration.GHOST_INKY_CHARACTER];
+
+   static GHOST_CHARACTERS = [Configuration.GHOST_BLINKY_CHARACTER,
+                              Configuration.GHOST_PINKY_CHARACTER,
+                              Configuration.GHOST_CLYDE_CHARACTER,
+                              Configuration.GHOST_INKY_CHARACTER];
+
+   static TELEPORTER_CHARACTERS = [Configuration.TELEPORTER_1_CHARACTER,
+                                   Configuration.TELEPORTER_2_CHARACTER,
+                                   Configuration.TELEPORTER_3_CHARACTER];
+
+   static PACMAN_INACCESSIBLE_TILES = [Configuration.WALL_CHARACTER, 
+                                       Configuration.GHOST_DOOR_CHARACTER];
+
+   
+
+   // Key Codes for user commands (Source: https://keycode.info/)
+   static KEY_CODE_LEFT_ARROW = 37;
+   static KEY_CODE_UP_ARROW = 38;
+   static KEY_CODE_RIGHT_ARROW = 39;
+   static KEY_CODE_DOWN_ARROW = 40;
+   static KEY_CODE_A = 65;
+   static KEY_CODE_D = 68;
+   static KEY_CODE_S = 83;
+   static KEY_CODE_W = 87;
+
 
 
    // Settings for view.js
-   static suffix_foreground_div = "fg";
-   static suffix_background_div = "bg";
-   static dimension_background_div_in_px = 30;
+   static SUFFIX_FOREGROUND_DIV = "fg";
+   static SUFFIX_BACKGROUND_DIV = "bg";
+   static DIMENSION_BACKGROUND_DIV_IN_PX = 30;
 
 
-   // Key Codes for pacman movement
-   // Source http://www.javascriptkeycode.com 
-   static key_code_left_arrow = 37;
-   static key_code_up_arrow = 38;
-   static key_code_right_arrow = 39;
-   static key_code_down_arrow = 40;
-   static key_code_a = 65;
-   static key_code_d = 68;
-   static key_code_s = 83;
-   static key_code_w = 87;
 
-   
-   // Code for board parsing
-   // Source: https://www.ascii-code.com/
-   static linefeed_code = 10;
-
-   static GHOST_MAX_RESPAWN_STAGE = 4;
-
-
-   static GHOST_CHARACTERS = [Configuration.ghost_blinky_character,
-                              Configuration.GHOST_PINKY_CHARACTER,
-                              Configuration.GHOST_CLYDE_CHARACTER,
-                              Configuration.GHOST_INKY_CHARACTER];
-
-   static ACTOR_CHARACTERS = [Configuration.pacman_character,
-                              Configuration.ghost_blinky_character,
-                              Configuration.GHOST_PINKY_CHARACTER,
-                              Configuration.GHOST_CLYDE_CHARACTER,
-                              Configuration.GHOST_INKY_CHARACTER];
-
-   static PACMAN_INACCESSIBLE_TILES = [Configuration.wall_character, 
-                                       Configuration.ghost_door_character];
-
-   static TELEPORTER_CHARACTERS = [Configuration.teleporter_1_tile_character,
-                                   Configuration.teleporter_2_tile_character,
-                                   Configuration.teleporter_3_tile_character];
+   // Others
+   static ID_UNACCESSIBLE_BOARD_TILES = -1;   // must be < 0
 
 
 }
