9ed4bc98d815486e0b63edb021217d1fca8de3a6
Lucas Vossen <lv_code@outlook.com>
2022-01-23 18:17:33 +0100

    format file

--- js/StyleClassMapper.mjs
+++ js/StyleClassMapper.mjs
@@ -7,40 +7,36 @@ export default class StyleClassMapper {
 
 
     static background_class_map = {
-        [Configuration.WALL_CHARACTER]:                 Configuration.WALL_BACKGROUND_CSS_CLASS,
-        [Configuration.EMPTY_TILE_CHARACTER]:           Configuration.EMPTY_TILE_BACKGROUND_CSS_CLASS,
-        [Configuration.GHOST_DOOR_CHARACTER]:           Configuration.GHOST_DOOR_BACKGROUND_CSS_CLASS,
-        [Configuration.TELEPORTER_1_CHARACTER]:    Configuration.TELEPORTER_1_BACKGROUND_CSS_CLASS,
-        [Configuration.TELEPORTER_2_CHARACTER]:    Configuration.TELEPORTER_2_BACKGROUND_CSS_CLASS,
-        [Configuration.TELEPORTER_3_CHARACTER]:    Configuration.TELEPORTER_3_BACKGROUND_CSS_CLASS,
-        [Configuration.PACMAN_CHARACTER]:               Configuration.EMPTY_TILE_BACKGROUND_CSS_CLASS,
-        [Configuration.GHOST_BLINKY_CHARACTER]:         Configuration.EMPTY_TILE_BACKGROUND_CSS_CLASS,
-
-        [Configuration.GHOST_PINKY_CHARACTER]:          Configuration.EMPTY_TILE_BACKGROUND_CSS_CLASS,
-        [Configuration.GHOST_INKY_CHARACTER]:           Configuration.EMPTY_TILE_BACKGROUND_CSS_CLASS,
-        [Configuration.GHOST_CLYDE_CHARACTER]:          Configuration.EMPTY_TILE_BACKGROUND_CSS_CLASS,
-
-        [Configuration.POINT_CHARACTER]:                Configuration.EMPTY_TILE_BACKGROUND_CSS_CLASS,
-        [Configuration.POWERUP_CHARACTER]:              Configuration.EMPTY_TILE_BACKGROUND_CSS_CLASS
+        [Configuration.WALL_CHARACTER]:             Configuration.WALL_BACKGROUND_CSS_CLASS,
+        [Configuration.EMPTY_TILE_CHARACTER]:       Configuration.EMPTY_TILE_BACKGROUND_CSS_CLASS,
+        [Configuration.GHOST_DOOR_CHARACTER]:       Configuration.GHOST_DOOR_BACKGROUND_CSS_CLASS,
+        [Configuration.TELEPORTER_1_CHARACTER]:     Configuration.TELEPORTER_1_BACKGROUND_CSS_CLASS,
+        [Configuration.TELEPORTER_2_CHARACTER]:     Configuration.TELEPORTER_2_BACKGROUND_CSS_CLASS,
+        [Configuration.TELEPORTER_3_CHARACTER]:     Configuration.TELEPORTER_3_BACKGROUND_CSS_CLASS,
+        [Configuration.PACMAN_CHARACTER]:           Configuration.EMPTY_TILE_BACKGROUND_CSS_CLASS,
+        [Configuration.GHOST_BLINKY_CHARACTER]:     Configuration.EMPTY_TILE_BACKGROUND_CSS_CLASS,
+        [Configuration.GHOST_PINKY_CHARACTER]:      Configuration.EMPTY_TILE_BACKGROUND_CSS_CLASS,
+        [Configuration.GHOST_INKY_CHARACTER]:       Configuration.EMPTY_TILE_BACKGROUND_CSS_CLASS,
+        [Configuration.GHOST_CLYDE_CHARACTER]:      Configuration.EMPTY_TILE_BACKGROUND_CSS_CLASS,
+        [Configuration.POINT_CHARACTER]:            Configuration.EMPTY_TILE_BACKGROUND_CSS_CLASS,
+        [Configuration.POWERUP_CHARACTER]:          Configuration.EMPTY_TILE_BACKGROUND_CSS_CLASS
     };
   
   
     static foreground_class_map = {
-        [Configuration.WALL_CHARACTER]:                 Configuration.EMPTY_FOREGROUND_CSS_CLASS,
-        [Configuration.EMPTY_TILE_CHARACTER]:           Configuration.EMPTY_FOREGROUND_CSS_CLASS,
-        [Configuration.GHOST_DOOR_CHARACTER]:           Configuration.EMPTY_FOREGROUND_CSS_CLASS,
-        [Configuration.TELEPORTER_1_CHARACTER]:    Configuration.EMPTY_FOREGROUND_CSS_CLASS,
-        [Configuration.TELEPORTER_2_CHARACTER]:    Configuration.EMPTY_FOREGROUND_CSS_CLASS,
-        [Configuration.TELEPORTER_3_CHARACTER]:    Configuration.EMPTY_FOREGROUND_CSS_CLASS,
-        [Configuration.PACMAN_CHARACTER]:               `${Configuration.PACMAN_FOREGROUND_CSS_CLASS}_${Configuration.INITIAL_PACMAN_SPRITE_DIRECTION}`,
-        [Configuration.GHOST_BLINKY_CHARACTER]:         `${Configuration.GHOST_BLINKY_MOVEMENT_FOREGROUND_CSS_CLASS}_${Configuration.INITIAL_GHOST_SPRITES_DIRECTION}`,
-
-        [Configuration.GHOST_PINKY_CHARACTER]:          `${Configuration.GHOST_PINKY_MOVEMENT_FOREGROUND_CSS_CLASS}_${Configuration.INITIAL_GHOST_SPRITES_DIRECTION}`,
-        [Configuration.GHOST_INKY_CHARACTER]:           `${Configuration.GHOST_INKY_MOVEMENT_FOREGROUND_CSS_CLASS}_${Configuration.INITIAL_GHOST_SPRITES_DIRECTION}`,
-        [Configuration.GHOST_CLYDE_CHARACTER]:          `${Configuration.GHOST_CLYDE_MOVEMENT_FOREGROUND_CSS_CLASS}_${Configuration.INITIAL_GHOST_SPRITES_DIRECTION}`,
-
-        [Configuration.POINT_CHARACTER]:                Configuration.POINT_FOREGROUND_CSS_CLASS,
-        [Configuration.POWERUP_CHARACTER]:              Configuration.POWERUP_FOREGROUND_CSS_CLASS
+        [Configuration.WALL_CHARACTER]:             Configuration.EMPTY_FOREGROUND_CSS_CLASS,
+        [Configuration.EMPTY_TILE_CHARACTER]:       Configuration.EMPTY_FOREGROUND_CSS_CLASS,
+        [Configuration.GHOST_DOOR_CHARACTER]:       Configuration.EMPTY_FOREGROUND_CSS_CLASS,
+        [Configuration.TELEPORTER_1_CHARACTER]:     Configuration.EMPTY_FOREGROUND_CSS_CLASS,
+        [Configuration.TELEPORTER_2_CHARACTER]:     Configuration.EMPTY_FOREGROUND_CSS_CLASS,
+        [Configuration.TELEPORTER_3_CHARACTER]:     Configuration.EMPTY_FOREGROUND_CSS_CLASS,
+        [Configuration.PACMAN_CHARACTER]:           `${Configuration.PACMAN_FOREGROUND_CSS_CLASS}_${Configuration.INITIAL_PACMAN_SPRITE_DIRECTION}`,
+        [Configuration.GHOST_BLINKY_CHARACTER]:     `${Configuration.GHOST_BLINKY_MOVEMENT_FOREGROUND_CSS_CLASS}_${Configuration.INITIAL_GHOST_SPRITES_DIRECTION}`,
+        [Configuration.GHOST_PINKY_CHARACTER]:      `${Configuration.GHOST_PINKY_MOVEMENT_FOREGROUND_CSS_CLASS}_${Configuration.INITIAL_GHOST_SPRITES_DIRECTION}`,
+        [Configuration.GHOST_INKY_CHARACTER]:       `${Configuration.GHOST_INKY_MOVEMENT_FOREGROUND_CSS_CLASS}_${Configuration.INITIAL_GHOST_SPRITES_DIRECTION}`,
+        [Configuration.GHOST_CLYDE_CHARACTER]:      `${Configuration.GHOST_CLYDE_MOVEMENT_FOREGROUND_CSS_CLASS}_${Configuration.INITIAL_GHOST_SPRITES_DIRECTION}`,
+        [Configuration.POINT_CHARACTER]:            Configuration.POINT_FOREGROUND_CSS_CLASS,
+        [Configuration.POWERUP_CHARACTER]:          Configuration.POWERUP_FOREGROUND_CSS_CLASS
     };
   
   
