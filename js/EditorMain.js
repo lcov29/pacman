6f043a7b9e5e827e68ff29ee3fa0b527eeee4bcd
Lucas Vossen <lv_code@outlook.com>
2022-01-31 22:41:47 +0100

    refactor to use states

--- js/EditorMain.js
+++ js/EditorMain.js
@@ -1,51 +1,55 @@
 "use strict";
 
 import LevelEditor from "./LevelEditor.mjs";
+import LevelEditorTileManipulationState from "./LevelEditorTileManipulationState.mjs";
+import LevelEditorScatterSelectionState from "./LevelEditorScatterSelectionState.mjs";
+import LevelEditorMapDimensionChangeState from "./LevelEditorMapDimensionChangeState.mjs";
 
 
 let editor = new LevelEditor();
 editor.setEditorContainer("editor_container");
 editor.setReferenceInputMapHeight("map_height");
 editor.setReferenceInputMapWidth("map_width");
-editor.setScatterPositionsContainer("scatter_position_container");
-editor.setOptionalSpawnPositionsContainer("spawn_position_container");
 editor.initialize();
-editor.handleMapDimensionChange(levelTileMouseoverCallback,
-                                scatterSelectionTileMouseenterCallback,
-                                scatterSelectionTileMouseleaveCallback, 
-                                levelTileClickCallback);
+buttonMapDimensionChangeCallback();
                                 
 
-// add handlers to level element selectors in selector bar
+
+// add handlers to level elements in selector bar
 let radios = document.querySelectorAll('input[name="selectors"]');
 for (let radio of radios) {
-    radio.addEventListener('click', selectTileTypeCallback);
+    radio.addEventListener('click', radioButtonTileSelectionCallback);
 }
+
 
 
 // add handlers to editor container (enable drawing level elements while keeping the mouse button pressed)
 let editor_container = document.getElementById('editor_container');
-editor_container.addEventListener('mousedown', function() { editor.setMousePressedStatus(true); });
-editor_container.addEventListener('mouseup', function() { editor.setMousePressedStatus(false); });
-editor_container.addEventListener('mouseleave', function() { editor.setMousePressedStatus(false);});
+editor_container.addEventListener('mousedown', editorContainerMouseDownCallback);
+editor_container.addEventListener('mouseup', editorContainerMouseUpCallback);
+editor_container.addEventListener('mouseleave', editorContainerMouseLeaveCallback);
+
 
 
 // add handler to map dimension apply button
-document.getElementById('button_map_sizer').addEventListener('click', changeMapDimension);
+document.getElementById('button_map_sizer').addEventListener('click', buttonMapDimensionChangeCallback);
+
 
 
 // add handlers to scatter select buttons
 let scatter_buttons = document.querySelectorAll('button[name="button_scatter_position"]');
 for (let scatter_button of scatter_buttons) {
-    scatter_button.addEventListener('click', scatterSelectionButtonCallback);
+    scatter_button.addEventListener('click', buttonScatterSelectionCallback);
 }
+
 
 
 // add handlers to spawn select buttons
 let spawn_buttons = document.querySelectorAll('button[name="button_spawn_position"]');
 for (let spawn_button of spawn_buttons) {
-    spawn_button.addEventListener('click', spawnSelectionButtonCallback);
+    spawn_button.addEventListener('click', buttonSpawnSelectionCallback);
 }
+
 
 
 // add handler to play button
@@ -54,49 +58,62 @@ document.getElementById('play_level').addEventListener('click', function() { edi
 
 
 
-
-
-
-
-// wrapper functions for callbacks implemented by LevelEditor
-function selectTileTypeCallback() {
-    editor.handleTileTypeSelectionCallback(this.id);
+// Callback wrappers
+function editorContainerMouseDownCallback() {
+    editor.handleEditorContainerMouseDown(this.id);
 }
 
 
-function changeMapDimension() {
-    editor.handleMapDimensionChange(levelTileMouseoverCallback,
-                                    scatterSelectionTileMouseenterCallback,
-                                    scatterSelectionTileMouseleaveCallback, 
-                                    levelTileClickCallback);
+function editorContainerMouseUpCallback() {
+    editor.handleEditorContainerMouseUp(this.id);
 }
 
 
-function scatterSelectionButtonCallback() {
-    editor.scatterSelectionButtonCallback(this.id);
+function editorContainerMouseLeaveCallback() {
+    editor.handleEditorContainerMouseLeave(this.id);
 }
 
 
-function spawnSelectionButtonCallback() {
-    editor.spawnSelectionButtonCallback(this.id);
+function editorTileClickCallback() {
+    editor.handleEditorTileClick(this.id);
 }
 
 
-function scatterSelectionTileMouseenterCallback() {
-    editor.handleScatterSelectionMouseenter(this.id);
+function editorTileMouseOverCallback() {
+    editor.handleEditorTileMouseOver(this.id);
 }
 
 
-function scatterSelectionTileMouseleaveCallback() {
-    editor.handleScatterSelectionMouseleave(this.id);
+function editorTileMouseEnterCallback() {
+    editor.handleEditorTileMouseEnter(this.id);
 }
 
 
-function levelTileClickCallback() {
-    editor.levelTileClickCallback(this.id);
+function editorTileMouseLeaveCallback() {
+    editor.handleEditorTileMouseLeave(this.id);
 }
 
 
-function levelTileMouseoverCallback() {
-    editor.levelTileMouseoverCallback(this.id);
+function radioButtonTileSelectionCallback() {
+    editor.setState(new LevelEditorTileManipulationState(this.id));
+}
+
+
+function buttonMapDimensionChangeCallback() {
+    let new_state = new LevelEditorMapDimensionChangeState();
+    new_state.setEditorTileClickCallback(editorTileClickCallback);
+    new_state.setEditorTileMouseEnterCallback(editorTileMouseEnterCallback);
+    new_state.setEditorTileMouseLeaveCallback(editorTileMouseLeaveCallback);
+    new_state.setEditorTileMouseOverCallback(editorTileMouseOverCallback);
+    editor.setState(new_state);
+}
+
+
+function buttonScatterSelectionCallback() {
+    editor.setState(new LevelEditorScatterSelectionState(this.id));
+}
+
+
+function buttonSpawnSelectionCallback() {
+    window.alert("implement scatter selection");
 }
--- js/LevelEditor.mjs
+++ js/LevelEditor.mjs
@@ -2,6 +2,7 @@
 
 import Configuration from "./Configuration.mjs";
 import LevelEditorInternalBoard from "./LevelEditorInternalBoard.mjs";
+import LevelEditorDefaultState from "./LevelEditorDefaultState.mjs";
 
 
 export default class LevelEditor {
@@ -9,23 +10,18 @@ export default class LevelEditor {
 
     constructor() {
         this.editor_container = null;
-        this.scatter_positions_container = null;
-        this.optional_spawn_positions_container = null;
-        this.input_map_width = null;
-        this.input_map_height = null;
         this.internal_board = new LevelEditorInternalBoard();
         this.level_scatter_positions = [];
         this.level_optional_spawn_positions = [];
         this.mapTileTypeToInternalElement = [];
         this.mapButtonIdToInputId = [];
-        this.currently_selected_tile_type = "";
-        this.currently_active_scatter_input = null;
-        this.currently_active_spawn_input = null;
-        this.is_mouse_pressed_inside_editor_area = false;
+        this.mapButtonIdToGhostCharacter = [];
+        this.mapInternalElementToScatterSpawnControlIds = [];
         this.is_ghost_blinky_scatter_spawn_control_displayed = false;
         this.is_ghost_pinky_scatter_spawn_control_displayed = false;
         this.is_ghost_clyde_scatter_spawn_control_displayed = false;
         this.is_ghost_inky_scatter_spawn_control_displayed = false;
+        this.current_state = null;
     }
 
 
@@ -33,6 +29,8 @@ export default class LevelEditor {
         let width = this.input_map_width.value;
         let height = this.input_map_height.value;
         this.internal_board.initialize(width, height);
+        this.current_state = new LevelEditorDefaultState();
+
         this.mapTileTypeToInternalElement = {
             'wall_tile':                    Configuration.WALL_CHARACTER,
             'empty_tile':                   Configuration.EMPTY_TILE_CHARACTER,
@@ -61,15 +59,28 @@ export default class LevelEditor {
             'select_spawn_position_ghost_clyde':        'spawn_position_ghost_clyde'
         };
         this.mapButtonIdToGhostCharacter = {
-            'scatter_position_ghost_blinky':     Configuration.GHOST_BLINKY_CHARACTER,
-            'scatter_position_ghost_pinky':      Configuration.GHOST_PINKY_CHARACTER,
-            'scatter_position_ghost_inky':       Configuration.GHOST_INKY_CHARACTER,
-            'scatter_position_ghost_clyde':      Configuration.GHOST_CLYDE_CHARACTER,
-            'spawn_position_ghost_blinky':       Configuration.GHOST_BLINKY_CHARACTER,
-            'spawn_position_ghost_pinky':        Configuration.GHOST_PINKY_CHARACTER,
-            'spawn_position_ghost_inky':         Configuration.GHOST_INKY_CHARACTER,
-            'spawn_position_ghost_clyde':        Configuration.GHOST_CLYDE_CHARACTER
+            'select_scatter_position_ghost_blinky':     Configuration.GHOST_BLINKY_CHARACTER,
+            'select_scatter_position_ghost_pinky':      Configuration.GHOST_PINKY_CHARACTER,
+            'select_scatter_position_ghost_inky':       Configuration.GHOST_INKY_CHARACTER,
+            'select_scatter_position_ghost_clyde':      Configuration.GHOST_CLYDE_CHARACTER,
+            'select_spawn_position_ghost_blinky':       Configuration.GHOST_BLINKY_CHARACTER,
+            'select_spawn_position_ghost_pinky':        Configuration.GHOST_PINKY_CHARACTER,
+            'select_spawn_position_ghost_inky':         Configuration.GHOST_INKY_CHARACTER,
+            'select_spawn_position_ghost_clyde':        Configuration.GHOST_CLYDE_CHARACTER
         }
+        this.mapInternalElementToScatterSpawnControlIds = {
+            [Configuration.GHOST_BLINKY_CHARACTER]: ["scatter_control_ghost_blinky", "spawn_control_ghost_blinky"],
+            [Configuration.GHOST_PINKY_CHARACTER]:  ["scatter_control_ghost_pinky", "spawn_control_ghost_pinky"],
+            [Configuration.GHOST_CLYDE_CHARACTER]:  ["scatter_control_ghost_clyde", "spawn_control_ghost_clyde"],
+            [Configuration.GHOST_INKY_CHARACTER]:   ["scatter_control_ghost_inky", "spawn_control_ghost_inky"]
+        };
+    }
+
+
+    setState(state) {
+        this.current_state.exit();
+        this.current_state = state;
+        this.current_state.initialize(this);
     }
 
 
@@ -78,13 +89,9 @@ export default class LevelEditor {
     }
 
 
-    setScatterPositionsContainer(id) {
-        this.scatter_positions_container = document.getElementById(id);
-    }
-
-
-    setOptionalSpawnPositionsContainer(id) {
-        this.optional_spawn_positions_container = document.getElementById(id);
+    setEditorContainerDimension(width, height) {
+        this.editor_container.style.width = `${width * 32}px`;
+        this.editor_container.style.height = `${height * 32}px`;
     }
 
 
@@ -95,36 +102,6 @@ export default class LevelEditor {
 
     setReferenceInputMapHeight(id) {
         this.input_map_height = document.getElementById(id);
-    }
-
-
-    setCurrentlySelectedTileType(tile_type) {
-        this.currently_selected_tile_type = tile_type;
-    }
-
-
-    setCurrentlyActiveScatterInput(button_id) {
-        if (button_id === '') {
-            this.currently_active_scatter_input = null;
-        } else {
-            let input_id = this.mapButtonIdToInputId[button_id];
-            this.currently_active_scatter_input = document.getElementById(input_id);
-        }
-    }
-
-
-    setCurrentlyActiveSpawnSelection(button_id) {
-        if (button_id === '') {
-            this.currently_active_spawn_input = null;
-        } else {
-            let input_id = this.mapButtonIdToInputId[button_id];
-            this.currently_active_spawn_input = document.getElementById(input_id);
-        }
-    }
-
-
-    setMousePressedStatus(status) {
-        this.is_mouse_pressed_inside_editor_area = status;
     }
 
 
@@ -154,244 +131,133 @@ export default class LevelEditor {
     }
 
 
-    getInternalElementForCurrentTileType() {
-        return this.mapTileTypeToInternalElement[this.currently_selected_tile_type];
+    getScatterInputIdFor(button_id) {
+        return this.mapButtonIdToInputId[button_id];
     }
 
 
-    getGhostCharacterForCurrentSelectionButton() {
-        let active_input = null;
-        if (this.currently_active_scatter_input === null) {
-            active_input = this.currently_active_spawn_input;
-        } else {
-            active_input = this.currently_active_scatter_input;
+    getCounterForGhostType(ghost_character) {
+        let counter = false;
+        switch(ghost_character) {
+            case Configuration.GHOST_BLINKY_CHARACTER:
+                counter = this.internal_board.getCounterGhostsBlinky();
+                break;
+            case Configuration.GHOST_PINKY_CHARACTER:
+                counter = this.internal_board.getCounterGhostsPinky();;
+                break;
+            case Configuration.GHOST_CLYDE_CHARACTER:
+                counter = this.internal_board.getCounterGhostsClyde();
+                break;
+            case Configuration.GHOST_INKY_CHARACTER:
+                counter = this.internal_board.getCounterGhostsInky();;
+                break;
         }
-        return this.mapButtonIdToGhostCharacter[active_input.id];
+        return counter;
+    }
+
+
+    getInternalElementFor(tile_id) {
+        return this.mapTileTypeToInternalElement[tile_id];
+    }
+
+
+    getGhostCharacterFor(button_id) {
+        return this.mapButtonIdToGhostCharacter[button_id];
+    }
+
+
+    getGhostCoordinatesListFor(ghost_character) {
+        return this.internal_board.getGhostCoordinatesListFor(ghost_character);
+    }
+
+
+    getScatterSpawnControlDisplayStatusForGhostType(ghost_character) {
+        let status = false;
+        switch(ghost_character) {
+            case Configuration.GHOST_BLINKY_CHARACTER:
+                status = this.is_ghost_blinky_scatter_spawn_control_displayed;
+                break;
+            case Configuration.GHOST_PINKY_CHARACTER:
+                status = this.is_ghost_pinky_scatter_spawn_control_displayed;
+                break;
+            case Configuration.GHOST_CLYDE_CHARACTER:
+                status = this.is_ghost_clyde_scatter_spawn_control_displayed;
+                break;
+            case Configuration.GHOST_INKY_CHARACTER:
+                status = this.is_ghost_inky_scatter_spawn_control_displayed;
+                break;
+        }
+        return status;
+    }
+
+
+    getScatterSpawnControlIDsForGhostType(ghost_character) {
+        return this.mapInternalElementToScatterSpawnControlIds[ghost_character];
+    }
+
+
+    getMapWidthInput() {
+        return this.input_map_width.value;
+    }
+
+
+    getMapHeightInput() {
+        return this.input_map_height.value;
     }
 
 
     // ======== IMPLEMENTATION OF CALLBACK FUNCTIONS ===========
-
-
-    levelTileClickCallback(caller_id) {
-        this.handleTileManipulationClick(caller_id);
-        this.handleScatterSelectionClick(caller_id);
-    }
-
-
-    levelTileMouseoverCallback(caller_id) {
-        this.handleTileManipuationMouseover(caller_id);
-    }
-
-
-    scatterSelectionButtonCallback(caller_id) {
-        this.resetHighlighOfChosenSelectorTile();
-        this.setCurrentlyActiveScatterInput(caller_id);
-        this.setCurrentlySelectedTileType('');
-        this.highlightPlacedGhosts();
-    }
-
-
-    spawnSelectionButtonCallback(caller_id) {
-        this.resetHighlighOfChosenSelectorTile();
-        this.setCurrentlyActiveSpawnSelection(caller_id);
-        this.setCurrentlySelectedTileType('');
-        this.highlightPlacedGhosts();
-    }
-
-
-    handleMapDimensionChange(callback_mousedown, callback_mouseenter, callback_mouseleave, callback_click) {
-        this.clearMap();
-        this.internal_board.initializeNewMap(this.input_map_width.value, this.input_map_height.value);
-        this.initializeEditingArea(callback_mousedown, callback_mouseenter, callback_mouseleave, callback_click);
-        this.resetSpawnScatterControlDisplayStatus();
-    }
-
-
-    handleTileTypeSelectionCallback(id) {
-        this.setCurrentlySelectedTileType(id);
-        this.highlightChosenSelectorTile();
-    }
-
-
-    handleTileManipulationClick(caller_id) {
-        if (this.currently_selected_tile_type !== '') {
-            let styleclass = `editor_tile ${this.currently_selected_tile_type}`;
-            document.getElementById(caller_id).setAttribute('class', styleclass);
-            let internal_element = this.getInternalElementForCurrentTileType();
-            this.internal_board.update(caller_id, internal_element);
-            this.manageScatterSpawnControlVisibility();
-            //this.internal_board.printInternalBoardToConsole();
-        }
-    }
-
-
-    handleTileManipuationMouseover(caller_id) {
-        if (this.is_mouse_pressed_inside_editor_area) {
-            this.handleTileManipulationClick(caller_id);
-        }
-    }
-
-
-    handleScatterSelectionMouseenter(caller_id) {
-        if (this.currently_active_scatter_input !== null) {
-            let ghost_character = this.getGhostCharacterForCurrentSelectionButton();
-            let ghost_coordinates = this.internal_board.getGhostCoordinatesListFor(ghost_character);
-            if (ghost_coordinates.includes(caller_id) === false) {
-                document.getElementById(caller_id).style.borderColor = 'green';
-                document.getElementById(caller_id).style.borderWidth = "5px";
-                this.currently_active_scatter_input.value = caller_id;
-            }
-        }
-    }
-
-
-    handleScatterSelectionMouseleave(caller_id) {
-        if (this.currently_active_scatter_input !== null) {
-            let ghost_character = this.getGhostCharacterForCurrentSelectionButton();
-            let ghost_coordinates = this.internal_board.getGhostCoordinatesListFor(ghost_character);
-            if (ghost_coordinates.includes(caller_id) === false) {
-                document.getElementById(caller_id).style = null;
-            }
-        }
-    }
-
-
-    handleScatterSelectionClick(caller_id) {
-        if (this.currently_active_scatter_input !== null) {
-            this.resetHighlightPlacedGhosts();
-            document.getElementById(caller_id).style = null;
-            this.currently_selected_tile_type = 'undefined_tile';
-            this.currently_active_scatter_input = null;
-        }       
-    }
-
-
-    handleSpawnSelection() {
-        window.alert("implement spawn selection");
-    }
-
-
-    handlePlay() {
-        window.alert("implement play");
-    }
-
-
-    highlightPlacedGhosts() {
-        let ghost_character = this.getGhostCharacterForCurrentSelectionButton();
-        let ghost_coordinates = this.internal_board.getGhostCoordinatesListFor(ghost_character);
-        for (let coordinate of ghost_coordinates) {
-            document.getElementById(coordinate).style.borderColor = 'red';
-            document.getElementById(coordinate).style.borderWidth = '5px';
-        }
-    }
-
-
-    resetHighlightPlacedGhosts() {
-        let ghost_character = this.getGhostCharacterForCurrentSelectionButton();
-        let ghost_coordinates = this.internal_board.getGhostCoordinatesListFor(ghost_character);
-        for (let coordinate of ghost_coordinates) {
-            document.getElementById(coordinate).style = null;
-        }
-    }  
     
-   
-    manageScatterSpawnControlVisibility() {
-        let ghost_type_counter = 0;
-        let ghost_type_control_display_status = false;
-        let ghost_type_control_ids = [];
 
-        for (let ghost_character of Configuration.GHOST_CHARACTERS) {
-
-            switch(ghost_character) {
-                case Configuration.GHOST_BLINKY_CHARACTER:
-                    ghost_type_counter = this.internal_board.getCounterGhostsBlinky();
-                    ghost_type_control_display_status = this.is_ghost_blinky_scatter_spawn_control_displayed;
-                    ghost_type_control_ids = ["scatter_control_ghost_blinky", "spawn_control_ghost_blinky"];
-                    break;
-                case Configuration.GHOST_PINKY_CHARACTER:
-                    ghost_type_counter = this.internal_board.getCounterGhostsPinky();
-                    ghost_type_control_display_status = this.is_ghost_pinky_scatter_spawn_control_displayed;
-                    ghost_type_control_ids = ["scatter_control_ghost_pinky", "spawn_control_ghost_pinky"];
-                    break;
-                case Configuration.GHOST_CLYDE_CHARACTER:
-                    ghost_type_counter = this.internal_board.getCounterGhostsClyde();
-                    ghost_type_control_display_status = this.is_ghost_clyde_scatter_spawn_control_displayed;
-                    ghost_type_control_ids = ["scatter_control_ghost_clyde", "spawn_control_ghost_clyde"];
-                    break;
-                case Configuration.GHOST_INKY_CHARACTER:
-                    ghost_type_counter = this.internal_board.getCounterGhostsInky();
-                    ghost_type_control_display_status = this.is_ghost_inky_scatter_spawn_control_displayed;
-                    ghost_type_control_ids = ["scatter_control_ghost_inky", "spawn_control_ghost_inky"];
-                    break;
-            }
-    
-            if ((ghost_type_counter) > 0 && (ghost_type_control_display_status === false)) {
-                for (let control_id of ghost_type_control_ids) {
-                    document.getElementById(control_id).style = null;
-                }
-                this.setSpawnScatterControlDisplayStatus(ghost_character, true);
-            }
-    
-            if ((ghost_type_counter === 0) && (ghost_type_control_display_status === true)) {
-                for (let control_id of ghost_type_control_ids) {
-                    document.getElementById(control_id).style = "display:none";
-                }
-                this.setSpawnScatterControlDisplayStatus(ghost_character, false);
-            }
-
-        }
+    handleEditorContainerMouseDown(caller_id) {
+        this.current_state.handleEditorContainerMouseDown(caller_id);
     }
 
 
-    highlightChosenSelectorTile() {
-        let radios = document.querySelectorAll('input[name="selectors"]');
-        let selected_radio_id = document.querySelector('input[name="selectors"]:checked').id;
-        for (let radio of radios) {
-            let radio_label = document.querySelector(`label[for="${radio.id}"]`);
-            if (selected_radio_id === radio.id) {
-                radio_label.style.borderColor = 'red';
-            } else {
-                radio_label.setAttribute('style', '');
-            }
-        }
+    handleEditorContainerMouseUp(caller_id) {
+        this.current_state.handleEditorContainerMouseUp(caller_id);
     }
 
 
-    resetHighlighOfChosenSelectorTile() {
-        let selected_radio_id = document.querySelector('input[name="selectors"]:checked').id;
-        let selected_radio_label = document.querySelector(`label[for="${selected_radio_id}"]`);
-        selected_radio_label.setAttribute('style', '');
+    handleEditorContainerMouseLeave(caller_id) {
+        this.current_state.handleEditorContainerMouseLeave(caller_id);
+    }
+
+
+    handleEditorTileClick(caller_id) {
+        this.current_state.handleEditorTileClick(caller_id);
+    }
+
+
+    handleEditorTileMouseOver(caller_id) {
+        this.current_state.handleEditorTileMouseOver(caller_id);
+    }
+
+
+    handleEditorTileMouseEnter(caller_id) {
+        this.current_state.handleEditorTileMouseEnter(caller_id);
+    }
+
+
+    handleEditorTileMouseLeave(caller_id) {
+        this.current_state.handleEditorTileMouseLeave(caller_id);
+    }
+
+
+    updateInternalBoard(tile_coordinates, element) {
+        let internal_element = this.getInternalElementFor(element);
+        this.internal_board.update(tile_coordinates, internal_element);
+    }
+
+
+    addEditorTile(new_tile) {
+        this.editor_container.appendChild(new_tile);
     }
 
 
     clearMap() {
         while (this.editor_container.firstChild) {
            this.editor_container.removeChild(this.editor_container.firstChild);
-        }
-    }
-
-
-    initializeEditingArea(callback_mouseover, callback_mouseenter, callback_mouseleave, callback_click) {
-        let width = this.input_map_width.value;
-        let height = this.input_map_height.value;
-        
-        this.editor_container.style.width = `${width * 32}px`;
-        this.editor_container.style.height = `${height * 32}px`;
-
-        for (let y = 0; y < height; y++) {
-            for (let x = 0; x < width; x++) {
-                let new_div = document.createElement('div');
-                let new_id = `(${x},${y})`;
-                new_div.setAttribute('id', new_id);
-                new_div.setAttribute('title', new_id);
-                new_div.setAttribute('class', 'editor_tile undefined_tile');
-                new_div.addEventListener('mouseover', callback_mouseover);
-                new_div.addEventListener('mouseenter', callback_mouseenter);
-                new_div.addEventListener('mouseleave', callback_mouseleave);
-                new_div.addEventListener('click', callback_click)
-                this.editor_container.appendChild(new_div);
-            }
         }
     }
 
