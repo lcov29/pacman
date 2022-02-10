b6cd224865c452eb7ea9f4259a2f7b2fb3691cde
Lucas Vossen <lv_code@outlook.com>
2022-02-10 21:46:26 +0100

    implement chase pattern

--- js/game/GhostPinky.mjs
+++ js/game/GhostPinky.mjs
@@ -18,8 +18,38 @@ export default class GhostPinky extends Ghost {
 
     // chase movement pattern implementation; is used by GhostStateChase
     calculateNextChasePosition(positionId) {
-        let pacmanId = super.selectClosestPacmanID();
-        return super.getRouting().calculateNextPositionOnShortestPath(positionId, pacmanId);
+        let targetTileId = this.calculateChaseTargetTileId();
+        return super.getRouting().calculateNextPositionOnShortestPath(positionId, targetTileId);
+    }
+
+
+    calculateChaseTargetTileId() {
+        let pacmanPositionId = super.selectClosestPacmanID();
+        let pacmanPosition = this.level.getPacmanPositionFor(pacmanPositionId);
+        let pacmanMovementDirection = this.level.getPacmanMovementDirectionFor(pacmanPositionId);
+        let currentTargetTileId = pacmanPositionId;
+        let x = pacmanPosition.getX();
+        let y =  pacmanPosition.getY();
+
+        for (let i = 0; i < Configuration.GHOST_PINKY_MAX_TILE_OFFSET_TO_PACMAN_DIRECTION_POSITION; i++) {
+            x += pacmanMovementDirection.x;
+            y += pacmanMovementDirection.y;
+            let calculatedPosition = this.level.getBoardPositionAt(x, y);
+            if (calculatedPosition === null) {
+                break;  // calculated position is outside of the board
+            } else {
+                if (this.isPositionAccessible(calculatedPosition)) {
+                    currentTargetTileId = calculatedPosition.getID();
+                }
+            }
+        }
+
+        return currentTargetTileId;
+    }
+
+
+    isPositionAccessible(position) {
+        return (Configuration.ACTORS_INACCESSIBLE_TILES.includes(position.getElementCharacter()) === false);
     }
 
     
