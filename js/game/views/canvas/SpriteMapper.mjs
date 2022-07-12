27f799863500d6c365679250abae3eee258fe780
Lucas Vossen <lv_code@outlook.com>
2022-07-12 19:46:00 +0200

    add file

---
+++ js/game/views/canvas/SpriteMapper.mjs
@@ -0,0 +1,18 @@
+import Configuration from "../../../Configuration.mjs";
+
+
+export default class SpriteMapper {
+
+/*
+    #initializeSpriteMapper() {
+        this.#spriteMapper = new Map();
+        this.#spriteMapper.set(Configuration.wallCharacter, document.getElementById('wall'));
+        this.#spriteMapper.set(Configuration.emptyTileCharacter, document.getElementById('emptySpace'));
+        this.#spriteMapper.set(Configuration.teleporter1Character, document.getElementById('teleporter1'));
+        this.#spriteMapper.set(Configuration.teleporter2Character, document.getElementById('teleporter2'))
+        this.#spriteMapper.set(Configuration.teleporter3Character, document.getElementById('teleporter3'))
+        this.#spriteMapper.set(Configuration.pointCharacter, document.getElementById('point'));
+        this.#spriteMapper.set(Configuration.powerUpCharacter, document.getElementById('powerUp'));
+    }*/
+    
+}
