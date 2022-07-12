fa22d6ea4f8ad3cd2d590799ee111aa41744e418
Lucas Vossen <lv_code@outlook.com>
2022-07-12 19:34:07 +0200

    refactor file

--- js/game/views/canvas/BackgroundCanvas.mjs
+++ js/game/views/canvas/BackgroundCanvas.mjs
@@ -1,104 +1,22 @@
-import Configuration from "../../../Configuration.mjs";
+import Canvas from "./Canvas.mjs";
 
 
-export default class BackgroundCanvas {
-
-
-    #canvas = null;
-    #context = null;
-    #backgroundRequestStack = null;
-    #spriteMapper = null;
-    #tileWidth = 0;
-    #tileHeight = 0;
-    #columnNumber = 0;
-    #rowNumber = 0;
+export default class BackgroundCanvas extends Canvas {
 
     
-    constructor() {
-        this.#canvas = document.getElementById('backgroundCanvas');
-        this.#context = this.#canvas.getContext('2d');
-        this.#backgroundRequestStack = [];
-        this.#initializeSpriteMapper();
-    }
-
-
-    set tileWidth(width) {
-        if (width < 1) {
-            throw new RangeError(`tileWidth must be greater than zero`);
-        } else {
-            this.#tileWidth = width;
-        }
-    }
-
-
-    set tileHeight(height) {
-        if (height < 1) {
-            throw new RangeError(`tileHeight must be greater than zero`);
-        } else {
-            this.#tileHeight = height;
-        }
-    }
-
-
-    set columnNumber(number) {
-        if (number < 1) {
-            throw new RangeError(`columnNumber must be greater than zero`);
-        } else {
-            this.#columnNumber = number;
-        }
-    }
-
-
-    set rowNumber(number) {
-        if (number < 1) {
-            throw new RangeError(`rowNumber must be greater than zero`);
-        } else {
-            this.#rowNumber = number;
-        }
-    }
-
-
-    resize() {
-        const canvasWidth = this.#tileWidth * this.#columnNumber;
-        const canvasHeight = this.#tileHeight * this.#rowNumber;
-        this.#canvas.width = canvasWidth;
-        this.#canvas.height = canvasHeight;
-    }
-
-
-    addRequest(backgroundRequest) {
-        this.#backgroundRequestStack.push(backgroundRequest);
-    }
-
-
-    processRequestStack() {
-        this.#backgroundRequestStack.forEach(this.drawOnBackground, this);
-        this.#backgroundRequestStack = [];
+    constructor(backgroundCanvas, spriteMapper) {
+        super(backgroundCanvas, spriteMapper);
     }
     
-    
-    drawOnBackground(request) {
-        const sprite = this.#spriteMapper.get(request.elementCharacter);
-        const xPosition = request.xPosition * this.#tileWidth;
-        const yPosition = request.yPosition * this.#tileHeight;
 
-        this.#context.save();
-        this.#context.translate(xPosition, yPosition);
-        this.#context.clearRect(0, 0, this.#tileWidth, this.#tileHeight);
-        this.#context.drawImage(sprite, 0, 0, this.#tileWidth, this.#tileHeight);
-        this.#context.restore();
+    processUpdateRequestStack() {
+        super.processUpdateRequestStack(this.drawBackgroundRequest, this);
     }
 
 
-    #initializeSpriteMapper() {
-        this.#spriteMapper = new Map();
-        this.#spriteMapper.set(Configuration.wallCharacter, document.getElementById('wall'));
-        this.#spriteMapper.set(Configuration.emptyTileCharacter, document.getElementById('emptySpace'));
-        this.#spriteMapper.set(Configuration.teleporter1Character, document.getElementById('teleporter1'));
-        this.#spriteMapper.set(Configuration.teleporter2Character, document.getElementById('teleporter2'))
-        this.#spriteMapper.set(Configuration.teleporter3Character, document.getElementById('teleporter3'))
-        this.#spriteMapper.set(Configuration.pointCharacter, document.getElementById('point'));
-        this.#spriteMapper.set(Configuration.powerUpCharacter, document.getElementById('powerUp'));
+    drawBackgroundRequest(request) {
+        const sprite = super.mapBackgroundToSprite(request.elementCharacter);
+        super.drawSprite(request.xPosition, request.yPosition, sprite);
     }
 
 
