55b47b3afa8ec1694e1150c4b3738312e26bf04e
Lucas Vossen <lv_code@outlook.com>
2022-07-14 10:54:47 +0200

    add call of MainCanvas.initializeAnimationObjectList() to method initialize()

--- js/game/views/canvas/CanvasView.mjs
+++ js/game/views/canvas/CanvasView.mjs
@@ -22,6 +22,7 @@ export default class CanvasView {
 
     initialize() {
         this.#initializeCanvasSize();
+        this.#mainCanvas.initializeAnimationObjectList();
         this.processRequestStacks();
         this.#mainCanvas.drawCurrentLevelState();
     }
