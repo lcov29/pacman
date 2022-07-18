8ed9f1fa6d2be2efe5152edfe2c7c5870f0449d5
Lucas Vossen <lv_code@outlook.com>
2022-07-18 15:51:35 +0200

    add file

---
+++ js/game/GhostStateScaredStart.mjs
@@ -0,0 +1,22 @@
+import Configuration from "../Configuration.mjs";
+import GhostStateScared from "./GhostStateScared.mjs";
+import GhostStateScaredEnd from "./GhostStateScaredEnd.mjs";
+
+
+export default class GhostStateScaredStart extends GhostStateScared {
+
+
+    constructor(ghost) {
+        super(ghost);
+        super.setName(Configuration.nameGhostStateScared);       
+        super.setDurationInTurns(Configuration.turnDurationGhostStateScared);
+        ghost.reverseCurrentMovementDirection();
+    }
+
+
+    getSubsequentState() {
+        return new GhostStateScaredEnd(super.getGhost());
+    }
+
+    
+}
