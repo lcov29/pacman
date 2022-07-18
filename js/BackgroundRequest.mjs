1efecbc35cb324828d77a852ee56787d09db1593
Lucas Vossen <lv_code@outlook.com>
2022-07-18 20:26:02 +0200

    add properties score and lifeCount with getter and setter methods

--- js/BackgroundRequest.mjs
+++ js/BackgroundRequest.mjs
@@ -7,6 +7,8 @@ export default class BackgroundRequest {
     #xPosition = 0;
     #yPosition = 0;
     #elementCharacter = '';
+    #score = -1;
+    #lifeCount = -1;
 
 
     constructor(xPosition, yPosition, elementCharacter) {
@@ -28,6 +30,34 @@ export default class BackgroundRequest {
 
     get elementCharacter() {
         return this.#elementCharacter;
+    }
+
+
+    get score() {
+        return this.#score;
+    }
+
+
+    set score(value) {
+        if (value > -1) {
+            this.#score = value;
+        } else {
+            throw new RangeError('score must be greater or equal zero');
+        }
+    }
+
+
+    get lifeCount() {
+        return this.#lifeCount;
+    }
+
+
+    set lifeCount(value) {
+        if (value > -1) {
+            this.#lifeCount = value;
+        } else {
+            throw new RangeError('life count must be greater or equal zero');
+        }
     }
 
 
