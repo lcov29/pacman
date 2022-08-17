12e2b0d44ba243f087f6e071081f6ee734f74ee4
Lucas Vossen <lv_code@outlook.com>
2022-08-17 10:52:40 +0200

    update import statement

--- js/editor/EditorInternalLevel.mjs
+++ js/editor/EditorInternalLevel.mjs
@@ -1,7 +1,7 @@
 'use strict';
 
-import Configuration from '../Configuration.mjs';
-import Utility from '../Utility.mjs';
+import Configuration from '../global/Configuration.mjs';
+import Utility from '../global/Utility.mjs';
 
 
 export default class EditorInternalLevel {
