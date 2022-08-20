e35329abf94808513617636c59fc90b2ca5ff05c
Lucas Vossen <lv_code@outlook.com>
2022-08-20 12:22:20 +0200

    declare properties as private

--- js/game/model/actors/Actor.mjs
+++ js/game/model/actors/Actor.mjs
@@ -8,134 +8,139 @@ import MovementRequest from '../../requests/MovementRequest.mjs';
 export default class Actor {
 
 
+   #level = null;
+   #currentPosition = null;
+   #nextPosition = null;
+   #character = '';
+   #movementDirectionName = '';
+   #hasTeleportedInPreviousTurn = false;
+   #spriteDisplayPriority = -1;
+
+
    constructor(level, position) {
-      this.level = level;
-      this.currentPosition = position;
-      this.nextPosition = position;
-      this.character = '';
-      this.movementDirectionName = '';
-      this.hasTeleportedInPreviousTurn = false;
-      this.spriteDisplayPriority = -1;
+      this.#level = level;
+      this.#currentPosition = position;
+      this.#nextPosition = position;
    }
 
 
    setSpriteDisplayPriority(priority) {
-      this.spriteDisplayPriority = priority;
+      this.#spriteDisplayPriority = priority;
    }
 
 
    setNextPosition(position) {
-      this.nextPosition = position;
+      this.#nextPosition = position;
    }
 
 
    setCharacter(character) {
-      this.character = character;
+      this.#character = character;
    }
 
 
    setMovementDirectionName(directionName) {
-      this.movementDirectionName = directionName;
+      this.#movementDirectionName = directionName;
    }
 
 
    setTeleportationStatus(status) {
-      this.hasTeleportedInPreviousTurn = status;
+      this.#hasTeleportedInPreviousTurn = status;
    }
 
 
    getCurrentPosition() {
-      return this.currentPosition;
+      return this.#currentPosition;
    }
 
 
    getNextPosition() {
-      return this.nextPosition;
+      return this.#nextPosition;
    }
 
 
    getCharacter() {
-      return this.character;
+      return this.#character;
    }
 
 
    getCurrentMovementDirectionName() {
-      return this.movementDirectionName;
+      return this.#movementDirectionName;
    }
 
 
    getCurrentMovementDirection() {
-      return Directions.getDirectionByName(this.movementDirectionName);
+      return Directions.getDirectionByName(this.#movementDirectionName);
    }
 
 
    getTeleportDestinationForCurrentPosition() {
-      return this.level.getTeleportDestination(this.currentPosition);
+      return this.#level.getTeleportDestination(this.#currentPosition);
    }
 
 
    isCurrentPositionActorCharacter(character) {
-      return this.currentPosition.actorLayerCharacter === character;
+      return this.#currentPosition.actorLayerCharacter === character;
    }
 
 
    isCurrentPositionElementCharacter(character) {
-      return this.currentPosition.elementLayerCharacter === character;
+      return this.#currentPosition.elementLayerCharacter === character;
    }
 
 
    isNextPositionActorCharacter(character) {
-      return this.nextPosition.actorLayerCharacter === character;
+      return this.#nextPosition.actorLayerCharacter === character;
    }
 
 
    isNextPositionElementCharacter(character) {
-      return this.nextPosition.elementLayerCharacter === character;
+      return this.#nextPosition.elementLayerCharacter === character;
    }
 
 
    isCurrentPositionTeleporter() {
-      let currentPositionElement = this.currentPosition.elementLayerCharacter;
+      let currentPositionElement = this.#currentPosition.elementLayerCharacter;
       return Configuration.teleporterCharacterList.includes(currentPositionElement);
    }
 
 
    isMovementDirectionSet() {
-      return this.movementDirectionName !== '';
+      return this.#movementDirectionName !== '';
    }
 
 
    incrementScoreBy(value) {
-      this.level.incrementScoreBy(value);
+      this.#level.incrementScoreBy(value);
    }
 
 
    sendLevelMovementRequest(ghostStateName) {
       const request = this.createMovementRequest(ghostStateName);
-      this.level.processMovementRequest(request);
+      this.#level.processMovementRequest(request);
    }
 
 
    sendLevelBackgroundRequest(request) {
-      this.level.processBackgroundRequest(request);
+      this.#level.processBackgroundRequest(request);
    }
 
 
    createMovementRequest(ghostStateName) {
       const request = new MovementRequest();
-      request.xPositionStart = this.currentPosition.x;
-      request.yPositionStart = this.currentPosition.y;
-      request.xPositionDestination = this.nextPosition.x;
-      request.yPositionDestination = this.nextPosition.y;
+      request.xPositionStart = this.#currentPosition.x;
+      request.yPositionStart = this.#currentPosition.y;
+      request.xPositionDestination = this.#nextPosition.x;
+      request.yPositionDestination = this.#nextPosition.y;
 
       const direction = this.getCurrentMovementDirection();
       request.xDirection = direction.x;
       request.yDirection = direction.y;
 
-      request.directionName = this.movementDirectionName;
-      request.actorCharacter = this.character;
-      request.isTeleportation = this.hasTeleportedInPreviousTurn;
-      request.spriteDisplayPriority = this.spriteDisplayPriority;
+      request.directionName = this.#movementDirectionName;
+      request.actorCharacter = this.#character;
+      request.isTeleportation = this.#hasTeleportedInPreviousTurn;
+      request.spriteDisplayPriority = this.#spriteDisplayPriority;
 
       if (ghostStateName) {
          request.actorStateName = ghostStateName;
@@ -146,22 +151,22 @@ export default class Actor {
 
 
    loadCurrentPositionFromBoard() {
-      let currentX = this.currentPosition.x;
-      let currentY = this.currentPosition.y;
-      this.currentPosition = this.level.getBoardPositionAt(currentX, currentY); 
+      let currentX = this.#currentPosition.x;
+      let currentY = this.#currentPosition.y;
+      this.#currentPosition = this.#level.getBoardPositionAt(currentX, currentY); 
    }
 
 
    loadNextPositionFromBoard() {
-      let nextX = this.nextPosition.x;
-      let nextY = this.nextPosition.y;
-      this.nextPosition = this.level.getBoardPositionAt(nextX, nextY);
+      let nextX = this.#nextPosition.x;
+      let nextY = this.#nextPosition.y;
+      this.#nextPosition = this.#level.getBoardPositionAt(nextX, nextY);
    }
 
 
    updateCurrentPosition() {
-      this.currentPosition = this.nextPosition;
-      this.nextPosition = null;
+      this.#currentPosition = this.#nextPosition;
+      this.#nextPosition = null;
    }
 
 
@@ -171,7 +176,7 @@ export default class Actor {
       let nextY = this.getCurrentPosition().y + direction.y;
       let nextPosition = null;
       try {
-         nextPosition =  this.level.getBoardPositionAt(nextX, nextY);
+         nextPosition =  this.#level.getBoardPositionAt(nextX, nextY);
       } catch(e) {
          // prevent actor from leaving the board
          nextPosition = this.getCurrentPosition();
