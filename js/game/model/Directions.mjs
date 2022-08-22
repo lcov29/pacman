23eba839fbf3acf1a8e4f17a9b9ee81f7b1b9e0d
Lucas Vossen <lv_code@outlook.com>
2022-08-22 12:00:33 +0200

    refactor class

--- js/game/model/Directions.mjs
+++ js/game/model/Directions.mjs
@@ -17,81 +17,77 @@ export default class Directions {
     static validDirectionNameList = [Configuration.directionNameUp, Configuration.directionNameRight,
                                      Configuration.directionNameDown, Configuration.directionNameLeft];
 
-
-    static directionMapNameToDirection = {
-        [Configuration.directionNameUp]:    this.directionUp,
-        [Configuration.directionNameRight]: this.directionRight,
-        [Configuration.directionNameDown]:  this.directionDown,
-        [Configuration.directionNameLeft]:  this.directionLeft
-    };
+    static directionNameToDirectionMap = new Map();
+    static directionNameInverseMap = new Map();
+    static directionToNameMap = new Map();
+    static directionIdToNameMap = new Map();
+    static directionIdToDirectionMap = new Map();
 
 
-    static directionMapInverse = {
-        [Configuration.directionNameUp]:      Configuration.directionNameDown,
-        [Configuration.directionNameRight]:   Configuration.directionNameLeft,
-        [Configuration.directionNameDown]:    Configuration.directionNameUp,
-        [Configuration.directionNameLeft]:    Configuration.directionNameRight
-    };
+    static initializeDirectionMaps() {
+        Directions.directionNameToDirectionMap.set(Configuration.directionNameUp, Directions.directionUp);
+        Directions.directionNameToDirectionMap.set(Configuration.directionNameRight, Directions.directionRight);
+        Directions.directionNameToDirectionMap.set(Configuration.directionNameDown, Directions.directionDown);
+        Directions.directionNameToDirectionMap.set(Configuration.directionNameLeft, Directions.directionLeft);
 
+        Directions.directionNameInverseMap.set(Configuration.directionNameUp, Configuration.directionNameDown);
+        Directions.directionNameInverseMap.set(Configuration.directionNameRight, Configuration.directionNameLeft);
+        Directions.directionNameInverseMap.set(Configuration.directionNameDown, Configuration.directionNameUp);
+        Directions.directionNameInverseMap.set(Configuration.directionNameLeft, Configuration.directionNameRight);
 
-    static directionMapDirectionToName = {
-        '(0,-1)': Configuration.directionNameUp,
-        '(1,0)':  Configuration.directionNameRight,
-        '(0,1)':  Configuration.directionNameDown,
-        '(-1,0)': Configuration.directionNameLeft
-    };
+        Directions.directionToNameMap.set('(0,-1)', Configuration.directionNameUp);
+        Directions.directionToNameMap.set('(1,0)', Configuration.directionNameRight);
+        Directions.directionToNameMap.set('(0,1)', Configuration.directionNameDown);
+        Directions.directionToNameMap.set('(-1,0)', Configuration.directionNameLeft);
 
+        Directions.directionIdToNameMap.set(0, Configuration.directionNameUp);
+        Directions.directionIdToNameMap.set(1, Configuration.directionNameRight);
+        Directions.directionIdToNameMap.set(2, Configuration.directionNameDown);
+        Directions.directionIdToNameMap.set(3, Configuration.directionNameLeft);
 
-    static directionMapIdToName = {
-        0:  Configuration.directionNameUp,
-        1:  Configuration.directionNameRight,
-        2:  Configuration.directionNameDown,
-        3:  Configuration.directionNameLeft
+        Directions.directionIdToDirectionMap.set(0, Directions.directionUp);
+        Directions.directionIdToDirectionMap.set(1, Directions.directionRight);
+        Directions.directionIdToDirectionMap.set(2, Directions.directionDown);
+        Directions.directionIdToDirectionMap.set(3, Directions.directionLeft);
     }
-
-
-    static directionMapIdToDirection = {
-        0: this.directionUp,
-        1: this.directionRight,
-        2: this.directionDown,
-        3: this.directionLeft
-    };
     
 
     static getMinDirectionID() {
-        return this.minDirectionId;
+        return Directions.minDirectionId;
     }
 
 
     static getMaxDirectionID() {
-        return this.maxDirectionId;
+        return Directions.maxDirectionId;
     }
 
 
     static getDirectionByName(name) {
-        return this.directionMapNameToDirection[name];
+        return Directions.directionNameToDirectionMap.get(name);
     }
 
 
     static getDirectionNameByIndex(x, y) {
         const index = `(${x},${y})`;
-        return this.directionMapDirectionToName[index];
+        return Directions.directionToNameMap.get(index);
     }
 
 
     static getReversedDirectionName(name) {
-        return this.directionMapInverse[name];
+        return Directions.directionNameInverseMap.get(name);
     }
 
 
     static getDirectionByID(directionId) {
-        return this.directionMapIdToDirection[directionId];
+        return Directions.directionIdToDirectionMap.get(directionId);
     }
 
 
     static getRandomDirectionName() {
-        const directionId = Utility.getRandomIntegerBetweenInclusive(this.minDirectionId, this.maxDirectionId);
-        return this.directionMapIdToName[directionId];
+        const minDirectionId = Directions.minDirectionId;
+        const maxDirectionId = Directions.maxDirectionId;
+        const directionId = Utility.getRandomIntegerBetweenInclusive(minDirectionId, maxDirectionId);
+        return Directions.directionIdToNameMap.get(directionId);
     }
 
     
