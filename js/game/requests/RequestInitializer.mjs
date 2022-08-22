67b98754eb1133e243563965cfec853de7d60cf5
Lucas Vossen <lv_code@outlook.com>
2022-08-22 06:31:42 +0200

    add file

---
+++ js/game/requests/RequestInitializer.mjs
@@ -0,0 +1,54 @@
+import BackgroundRequest from "./BackgroundRequest.mjs";
+import MovementRequest from "./MovementRequest.mjs";
+import Configuration from "../../global/Configuration.mjs";
+
+
+export default class RequestInitializer {
+
+
+    static buildInitialBackgroundRequestList(boardPositionArray, score, lifeCount) {
+        const requestList = [];
+
+        for (let row of boardPositionArray) {
+            for (let element of row) {
+                const request = new BackgroundRequest(element.x, element.y, element.elementLayerCharacter);
+                request.score = score;
+                request.lifeCount = lifeCount;
+                requestList.push(request);
+            }
+        }
+
+        return requestList;
+    }
+
+
+    static buildInitialActorMovementRequestList(initialActorPositionList) {
+        const requestList = [];
+
+        for (let position of initialActorPositionList) {
+            const request = new MovementRequest();
+
+            request.xPositionStart = position.x;
+            request.yPositionStart = position.y;
+            request.xPositionDestination = position.x;
+            request.yPositionDestination = position.y;
+
+            const actorCharacter = position.actorLayerCharacter;
+            request.actorCharacter =  actorCharacter;
+
+            const isGhostCharacter = Configuration.ghostCharacterList.includes(actorCharacter);
+            if (isGhostCharacter) {
+                request.directionName = Configuration.initialGhostSpriteDirection;
+                request.actorStateName = Configuration.initialGhostStateName;
+            } else {
+                request.directionName = Configuration.initialPacmanSpriteDirection;
+            }
+            
+            requestList.push(request);
+        }
+
+        return requestList;
+    }
+
+
+}
