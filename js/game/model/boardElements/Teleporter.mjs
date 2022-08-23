74f3b6d18c87c64425f794a98590ba510e1752ea
Lucas Vossen <lv_code@outlook.com>
2022-08-23 05:39:29 +0200

    refactor class

--- js/game/model/boardElements/Teleporter.mjs
+++ js/game/model/boardElements/Teleporter.mjs
@@ -3,29 +3,29 @@
 export default class Teleporter {
 
 
-    #positionTeleporter1 = null;
-    #positionTeleporter2 = null;
+    #position1 = null;
+    #position2 = null;
 
     
     constructor() {}
 
 
     get idPosition1() {
-        return this.#positionTeleporter1.id;
+        return this.#position1.id;
     }
 
 
     get idPosition2() {
-        return this.#positionTeleporter2.id;
+        return this.#position2.id;
     }
 
 
     getDestinationPositionFor(position) {
         switch(position.id) {
-            case this.#positionTeleporter1.id:
-                return this.#positionTeleporter2.clone();
-            case this.#positionTeleporter2.id:
-                return this.#positionTeleporter1.clone();
+            case this.#position1.id:
+                return this.#position2.clone();
+            case this.#position2.id:
+                return this.#position1.clone();
             default:
                 return null;
         }
@@ -33,24 +33,24 @@ export default class Teleporter {
 
 
     isInitialized() {
-        const isTeleporter1Initialized = this.#positionTeleporter1 !== null;
-        const isTeleporter2Initialized = this.#positionTeleporter2 !== null;
-        return isTeleporter1Initialized && isTeleporter2Initialized;
+        const isPosition1Initialized = this.#position1 !== null;
+        const isPosition2Initialized = this.#position2 !== null;
+        return isPosition1Initialized && isPosition2Initialized;
     }
 
     
     isTeleporter(position) {
-        const isTeleporter1 = position.id === this.#positionTeleporter1.id;
-        const isTeleporter2 = position.id === this.#positionTeleporter2.id;
-        return  isTeleporter1 || isTeleporter2;    
+        const isEqualToPosition1 = position.id === this.#position1.id;
+        const isEqualToPosition2 = position.id === this.#position2.id;
+        return  isEqualToPosition1 || isEqualToPosition2;    
     }
 
 
     add(position) {
-        if (this.#positionTeleporter1) {
-            this.#positionTeleporter2 = position;
+        if (this.#position1) {
+            this.#position2 = position;
         } else {
-            this.#positionTeleporter1 = position;
+            this.#position1 = position;
         }
     }
 
