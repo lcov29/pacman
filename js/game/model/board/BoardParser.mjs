a96044e62c7c2b88e712c0fb633bd930bf75e95e
Lucas Vossen <lv_code@outlook.com>
2022-08-25 22:58:27 +0200

    update methods

--- js/game/model/board/BoardParser.mjs
+++ js/game/model/board/BoardParser.mjs
@@ -18,41 +18,52 @@ export default class BoardParser {
     parse(levelJson) {
         const parsedLevelJson = JSON.parse(levelJson);
 
-        this.#initializeBoardElementPositionList(parsedLevelJson.scatterPositionList, this.#boardRef.ghostScatterPositionList);
-        this.#initializeBoardElementPositionList(parsedLevelJson.optionalSpawnList, this.#boardRef.ghostOptionalSpawnPositionList);
-        this.#initializeBoardElementPositionList(parsedLevelJson.bonusSpawnPositionList, this.#boardRef.bonusSpawnPositionList);
-        this.#initializeOtherPositionLists(parsedLevelJson.board);
-
         const board = this.#buildBoardPositionArray(parsedLevelJson.board);
         this.#indexAccessiblePositions(board);
         this.#boardRef.board = board;
+
+        this.#boardRef.ghostScatterPositionList = this.#buildBoardElementPositionList(parsedLevelJson.scatterPositionList, board);
+        this.#boardRef.ghostOptionalSpawnPositionList = this.#buildBoardElementPositionList(parsedLevelJson.optionalSpawnList, board);
+        this.#boardRef.bonusSpawnPositionList = this.#buildBoardElementPositionList(parsedLevelJson.bonusSpawnPositionList, board);
+        this.#initializeOtherPositionLists(parsedLevelJson.board);
     }
 
 
-    #initializeBoardElementPositionList(initialList, boardRefList) {
-        const elementCharacter = (position.ghost) ? position.ghost : 'bonusSpawn';
-        boardRefList = initialList.map((position) => new BoardPosition(position.x, position.y, elementCharacter));
+    #buildBoardElementPositionList(positionList, board) {
+        return positionList.map((position) => {
+            const elementCharacter = (position.ghost) ? position.ghost : '';
+            const boardPosition = new BoardPosition(position.x, position.y, elementCharacter);
+            boardPosition.id = board[position.y][position.x].id;
+            return boardPosition;
+        });
     }
 
 
     #initializeOtherPositionLists(board) {
         for (let y = 0; y < board.length; y++) {
            for (let x = 0; x < board[y].length; x++) {
-              const currentCharacter = board[y][x];
+              const elementCharacter = board[y][x];
+              const currentBoardPositionId = this.#boardRef.getPosition(x, y).id;
   
-              const isCurrentCharacterPacman = currentCharacter === Configuration.pacmanCharacter;
-              if (isCurrentCharacterPacman) {
-                 this.#boardRef.initialPacmanPositionList.push(new BoardPosition(x, y, currentCharacter));
+              const isCurrentActorPacman = elementCharacter === Configuration.pacmanCharacter;
+              if (isCurrentActorPacman) {
+                const boardPosition = new BoardPosition(x, y, elementCharacter);
+                boardPosition.id = currentBoardPositionId;
+                this.#boardRef.initialPacmanPositionList.push(boardPosition);
               }
 
-              const isCurrentCharacterGhost = Configuration.ghostCharacterList.includes(currentCharacter);
-              if (isCurrentCharacterGhost) {
-                this.#boardRef.initialGhostPositionList.push(new BoardPosition(x, y, currentCharacter));
+              const isCurrentActorGhost = Configuration.ghostCharacterList.includes(elementCharacter);
+              if (isCurrentActorGhost) {
+                const boardPosition = new BoardPosition(x, y, elementCharacter);
+                boardPosition.id = currentBoardPositionId;
+                this.#boardRef.initialGhostPositionList.push(boardPosition);
               }
   
-              const isCurrentCharacterTeleporter = Configuration.teleporterCharacterList.includes(currentCharacter);
-              if (isCurrentCharacterTeleporter) {
-                this.#boardRef.teleporterPositionList.push(new BoardPosition(x, y, currentCharacter));
+              const isCurrentElementTeleporter = Configuration.teleporterCharacterList.includes(elementCharacter);
+              if (isCurrentElementTeleporter) {
+                const boardPosition = new BoardPosition(x, y, elementCharacter);
+                boardPosition.id = currentBoardPositionId;
+                this.#boardRef.teleporterPositionList.push(boardPosition);
               }
            }
         }
