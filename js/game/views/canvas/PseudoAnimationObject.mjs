390d7f90b2df5a033ede5fd1d5cf01bef932267f
Lucas Vossen <lv_code@outlook.com>
2022-08-25 05:11:33 +0200

    add condition to method buildPseudoMovementRequest()

--- js/game/views/canvas/PseudoAnimationObject.mjs
+++ js/game/views/canvas/PseudoAnimationObject.mjs
@@ -8,23 +8,22 @@ export default class PseudoAnimationObject extends AnimationObject {
 
     constructor() {
         super(Configuration.spriteAlternationIntervalLength);
-
     }
 
 
-    loadPseudoMovementData(tileWidth, tileHeight) {
-        const pseudoMovementRequest = this.#buildPseudoMovementRequest();
+    loadPseudoMovementData(isLevelInitialization, tileWidth, tileHeight) {
+        const pseudoMovementRequest = this.#buildPseudoMovementRequest(isLevelInitialization);
         const mainSprite = document.getElementById('undefinedTile');
         const alternateSprite = null;
         super.load(pseudoMovementRequest, mainSprite, alternateSprite, tileWidth, tileHeight);
     }
 
 
-    #buildPseudoMovementRequest() {
+    #buildPseudoMovementRequest(isLevelInitialization) {
         const request = new MovementRequest();
         request.xPositionStart = 1;
         request.yPositionStart = 1;
-        request.xPositionDestination = 2;
+        request.xPositionDestination = (isLevelInitialization) ? 1 : 2;     // prevent delay of game start
         request.yPositionDestination = 1;
         request.xDirection = 1;
         request.yDirection = 0;
