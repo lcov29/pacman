4786aeb091ebc46ff31517881deb449b862fba66
Lucas Vossen <lv_code@outlook.com>
2022-08-27 16:31:00 +0200

    add file

---
+++ js/game/views/canvas/RespawnAnimationObject.mjs
@@ -0,0 +1,93 @@
+import Configuration from '../../../global/Configuration.mjs';
+
+
+export default class RespawnAnimationObject {
+
+
+    #xPosition = -1;
+    #yPosition = -1;
+    #currentRespawnStage = -1;
+    #maxRespawnStage = -1;
+    #currentRectangleHeightInPixel = -1;
+    #minRectangleHeightInPixel = -1
+    #tileHeightInPixel = -1;
+    #respawnAnimationSpeedInPixel = -1;
+
+
+    constructor(respawnRequest, tileHeight) {
+        this.#xPosition = respawnRequest.xPosition;
+        this.#yPosition = respawnRequest.yPosition;
+        this.#currentRespawnStage = respawnRequest.respawnStage;
+        this.#maxRespawnStage = Configuration.ghostMaxRespawnStage;
+        this.#tileHeightInPixel = tileHeight;
+
+        this.#currentRectangleHeightInPixel = this.#calculateStartRectangleHeightInPixel();
+        this.#minRectangleHeightInPixel = this.#calculateMinRectangleHeightInPixel();
+    }
+
+
+    get xPosition() {
+        return this.#xPosition;
+    }
+
+
+    get yPosition() {
+        return this.#yPosition;
+    }
+
+
+    get rectangleHeightInPixel() {
+        return this.#currentRectangleHeightInPixel;
+    }
+
+
+    convertActorMovementSpeed(actorMovementSpeedInPixel) {
+        const tileHeightPerRespawnStage =  this.#calculateTileHeightPerRespawnStageInPixel();
+        const numberOfAnimationIterations = this.#tileHeightInPixel / actorMovementSpeedInPixel;
+
+        this.#respawnAnimationSpeedInPixel = tileHeightPerRespawnStage / numberOfAnimationIterations;
+    }
+
+
+    isAnimationComplete() {
+        return this.#currentRectangleHeightInPixel === this.#minRectangleHeightInPixel;
+    }
+
+
+    move() {
+        if (!this.isAnimationComplete()) {
+            const calculatedRectangleHeightInPixel = this.#currentRectangleHeightInPixel - this.#respawnAnimationSpeedInPixel;
+            this.#currentRectangleHeightInPixel = this.#handleUnderrunMinRectangleHeight(calculatedRectangleHeightInPixel);
+        }
+    }
+
+
+    #calculateStartRectangleHeightInPixel() {
+        const numberOfRespawnStagesLeftBeforeAnimation = this.#maxRespawnStage - this.#currentRespawnStage;
+        const tileHeightPerRespawnStage = this.#calculateTileHeightPerRespawnStageInPixel();
+        
+        return numberOfRespawnStagesLeftBeforeAnimation * tileHeightPerRespawnStage;
+    }
+
+
+    #calculateMinRectangleHeightInPixel() {
+        const numberOfRespawnStagesLeftAfterAnimation = this.#maxRespawnStage - this.#currentRespawnStage - 1;
+        const tileHeightPerRespawnStage = this.#calculateTileHeightPerRespawnStageInPixel();
+
+        return numberOfRespawnStagesLeftAfterAnimation * tileHeightPerRespawnStage;
+    }
+
+
+    #calculateTileHeightPerRespawnStageInPixel() {
+        return this.#tileHeightInPixel / this.#maxRespawnStage;
+    }
+
+
+    #handleUnderrunMinRectangleHeight(calculatedRectangleHeightInPixel) {
+        const isMinRectangleHeightUnderrun = calculatedRectangleHeightInPixel < this.#minRectangleHeightInPixel;
+        const calculatedHeight = (isMinRectangleHeightUnderrun) ? this.#minRectangleHeightInPixel : calculatedRectangleHeightInPixel;
+        return calculatedHeight;
+    }
+
+
+}
