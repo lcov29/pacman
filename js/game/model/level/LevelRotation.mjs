4698febbda3b5d43df551afb0170a65edf4bcd6d
Lucas Vossen <lv_code@outlook.com>
2022-08-29 17:06:19 +0200

    add file

---
+++ js/game/model/level/LevelRotation.mjs
@@ -0,0 +1,63 @@
+import Configuration from "../../../global/Configuration.mjs";
+import Level from "./Level.mjs";
+
+
+export default class LevelRotation {
+
+
+    #jsonLevelRotationList = [];
+    #currentJsonLevelIndex = 0;
+    #currentJsonLevelRemainingIterations = 0;
+
+
+    constructor() {}
+
+
+    initialize() {
+        const customLevelRotationName = Configuration.customLevelRotationSessionStorageName;
+        let levelRotationString = window.sessionStorage.getItem(customLevelRotationName);
+  
+        if (levelRotationString) {
+           window.sessionStorage.removeItem(customLevelRotationName);
+        } else {
+           levelRotationString = Configuration.jsonDefaultLevel;
+        }
+  
+        this.#jsonLevelRotationList = JSON.parse(levelRotationString);
+        this.#currentJsonLevelRemainingIterations = this.#getCurrentLevelJson().numberOfIterations;
+    }
+    
+
+    getNextLevel() {
+        this.#updateCurrentJsonLevelIndex();
+        const level = new Level(this);
+        const levelJson = this.#getCurrentLevelJson();
+        level.initialize(levelJson);
+        return level;
+    }
+
+
+    #getCurrentLevelJson() {
+        return this.#jsonLevelRotationList[this.#currentJsonLevelIndex];
+    }
+
+
+    #updateCurrentJsonLevelIndex() {
+        const hasRemainingIterationsLeft = this.#currentJsonLevelRemainingIterations > 0;
+
+        if (hasRemainingIterationsLeft) {
+            this.#currentJsonLevelRemainingIterations--;
+            return;
+        }
+
+        const isLastLevelInRotation = this.#currentJsonLevelIndex === this.#jsonLevelRotationList.length;
+
+        if (isLastLevelInRotation) {
+            this.#currentJsonLevelIndex = 0;    // proceed with first level       
+        } else {
+            this.#currentJsonLevelIndex++;
+        }
+    }
+        
+
+}
