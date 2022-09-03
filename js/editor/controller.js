36c960af619e98e673df5fee52f6ac1c913ae573
Lucas Vossen <lv_code@outlook.com>
2022-09-03 10:46:10 +0200

    add base functions for level rotation bar

--- js/editor/controller.js
+++ js/editor/controller.js
@@ -10,7 +10,11 @@ import Configuration from '../global/Configuration.mjs';
 
 const editor = new Editor();
 buttonMapDimensionChangeCallback();
-                                
+
+
+
+// ========== Add Event Listeners ==========
+
 
 // add handlers to level elements in selector bar
 const radioList = document.querySelectorAll('input[name="selectors"]');
@@ -62,84 +66,12 @@ for (let spawnButton of spawnButtonList) {
 document.getElementById('playLevel').addEventListener('click', buttonPlayCallback);
 
 
+
+// ========== Callback Functions For Event Listeners ==========
+
+
 function radioButtonTileSelectionCallback(event) {
     editor.setState(new EditorTileManipulationState(event.target.id));
-}
-
-
-function validateMapWidthInput(event) {
-    const input = document.getElementById(event.target.id);
-    try {
-        const inputNumber = parseInt(input.value);
-        const isWidthInputBelowMinimum = inputNumber < Configuration.editorBoardMinWidth;
-        const isWidthInputAboveMaximum = inputNumber > Configuration.editorBoardMaxWidth;
-        const isWidthInputInvalid = isWidthInputBelowMinimum || isWidthInputAboveMaximum;
-        
-        if (isWidthInputInvalid) {
-            input.value = '';
-        } else {
-            input.value = inputNumber;
-        }
-    } catch(e) {
-        input.value = '';
-    }
-}
-
-
-function validateMapHeightInput(event) {
-    const input = document.getElementById(event.target.id);
-    try {
-        const inputNumber = parseInt(input.value);
-        const isHeightInputBelowMinimum = inputNumber < Configuration.editorBoardMinHeight;
-        const isHeightInputAboveMaximum = inputNumber > Configuration.editorBoardMaxHeight;
-        const isHeightInputInvalid = isHeightInputBelowMinimum || isHeightInputAboveMaximum;
-
-        if (isHeightInputInvalid) {
-            input.value = '';
-        } else {
-            input.value = inputNumber;
-        }
-    } catch(e) {
-        input.value = '';
-    }
-}
-
-
-function validateInitialLifeInput(event) {
-    const input = document.getElementById(event.target.id);
-    const inputNumber = parseInt(input.value);
-    const isInputValidNumber = !isNaN(inputNumber) && inputNumber > 0;
-
-    if (!isInputValidNumber) {
-        input.value = 1;
-    } else {
-        input.value = inputNumber;
-    }
-}
-
-
-function processLevelIterationInput(event) {
-    validateLevelIterationInput(event);
-    editor.updateCurrentLevelIterationNumber();
-}
-
-
-function validateLevelIterationInput(event) {
-    const input = document.getElementById(event.target.id);
-    const inputValue = input.value.toLowerCase();
-    const inputNumber = parseInt(inputValue);
-    const isInputValidNumber = !isNaN(inputNumber) && inputNumber > 0;
-    const isInputInfinity = inputValue === 'infinity';
-
-    if (!isInputValidNumber) {
-        if (isInputInfinity) {
-            input.value = "Infinity";
-        } else {
-            input.value = "1";
-        }
-    } else {
-        input.value = inputNumber;
-    }
 }
 
 
@@ -188,8 +120,210 @@ function buttonPlayCallback() {
 }
 
 
+function validateInitialLifeInput(event) {
+    const input = document.getElementById(event.target.id);
+    const inputNumber = parseInt(input.value);
+    const isInputValidNumber = !isNaN(inputNumber) && inputNumber > 0;
+
+    if (!isInputValidNumber) {
+        input.value = 1;
+    } else {
+        input.value = inputNumber;
+    }
+}
+
+
+function processLevelIterationInput(event) {
+    validateLevelIterationInput(event);
+    editor.updateCurrentLevelIterationNumber();
+}
+
+
+function validateMapWidthInput(event) {
+    const input = document.getElementById(event.target.id);
+    try {
+        const inputNumber = parseInt(input.value);
+        const isWidthInputBelowMinimum = inputNumber < Configuration.editorBoardMinWidth;
+        const isWidthInputAboveMaximum = inputNumber > Configuration.editorBoardMaxWidth;
+        const isWidthInputInvalid = isWidthInputBelowMinimum || isWidthInputAboveMaximum;
+        
+        if (isWidthInputInvalid) {
+            input.value = '';
+        } else {
+            input.value = inputNumber;
+        }
+    } catch(e) {
+        input.value = '';
+    }
+}
+
+
+function validateMapHeightInput(event) {
+    const input = document.getElementById(event.target.id);
+    try {
+        const inputNumber = parseInt(input.value);
+        const isHeightInputBelowMinimum = inputNumber < Configuration.editorBoardMinHeight;
+        const isHeightInputAboveMaximum = inputNumber > Configuration.editorBoardMaxHeight;
+        const isHeightInputInvalid = isHeightInputBelowMinimum || isHeightInputAboveMaximum;
+
+        if (isHeightInputInvalid) {
+            input.value = '';
+        } else {
+            input.value = inputNumber;
+        }
+    } catch(e) {
+        input.value = '';
+    }
+}
+
+
+// ========== Helper Functions ==========
+
+
+function validateLevelIterationInput(event) {
+    const input = document.getElementById(event.target.id);
+    const inputValue = input.value.toLowerCase();
+    const inputNumber = parseInt(inputValue);
+    const isInputValidNumber = !isNaN(inputNumber) && inputNumber > 0;
+    const isInputInfinity = inputValue === 'infinity';
+
+    if (!isInputValidNumber) {
+        if (isInputInfinity) {
+            input.value = "Infinity";
+        } else {
+            input.value = "1";
+        }
+    } else {
+        input.value = inputNumber;
+    }
+}
+
+
 // workaround to make loading of index.html on github pages possible
 function loadIndexPage() {
     const url = location.href;
     location.href = url.replace(Configuration.fileNameEditor, Configuration.fileNameIndex);
+}
+
+
+
+// ========== Level Rotation Bar ==========
+
+let nextUnusedLevelNumber = 1;
+
+addLevelCallback();
+
+
+const addButton = document.getElementById('levelAddButton');
+addButton.addEventListener('click', addLevelCallback);
+
+
+function addLevelCallback() {
+    const newLevelNode = buildNewLevelNode();
+    addDeleteButtonEventListener(newLevelNode);
+    addLevelSelectEventListener(newLevelNode);
+    insertLevelInRotation(newLevelNode);
+    highlightNewLevel();
+    setIterationNumberForSelectedLevel(5);
+}
+
+
+function buildNewLevelNode() {
+    const deepCopy = true;
+    const newLevelNode = document.getElementById('levelElementTemplate').cloneNode(deepCopy);
+    newLevelNode.setAttribute('id', `level${nextUnusedLevelNumber}`);
+    nextUnusedLevelNumber++;
+    return newLevelNode;
+}
+
+
+function addDeleteButtonEventListener(levelElement) {
+    const deleteButton = levelElement.children[1];
+    deleteButton.addEventListener('click', handleLevelDeletion);
+}
+
+
+function addLevelSelectEventListener(levelElement) {
+    levelElement.addEventListener('click', highlightSelectedLevel);
+}
+
+
+function insertLevelInRotation(levelElement) {
+    const levelRotation = document.getElementById('rotationContainer');
+    levelRotation.appendChild(levelElement);
+}
+
+
+function highlightNewLevel() {
+    const levelRotation = document.getElementById('rotationContainer');
+    const classNameSelectedLevel = 'levelSelected';
+
+    for(const levelElement of levelRotation.children) {
+        if (levelElement.nextElementSibling) {
+            levelElement.classList.remove(classNameSelectedLevel);
+        } else {
+            levelElement.classList.add(classNameSelectedLevel);
+        }
+    }
+}
+
+
+function setIterationNumberForSelectedLevel(iterationNumber) {
+    const selectedLevel = document.getElementsByClassName('levelSelected')[0];
+    const iterationDiv = selectedLevel.children[0];
+    const classInfiniteSymbol = 'displayInfiniteSymbol';
+    const isInfiniteIteration = iterationNumber === 'Infinity';
+
+    if (isInfiniteIteration) {
+        iterationDiv.classList.add(classInfiniteSymbol);
+        iterationDiv.innerText = '';
+    } else {
+        iterationDiv.classList.remove(classInfiniteSymbol);
+        iterationDiv.innerText = `${iterationNumber}x`;
+    }
+}
+
+
+function handleLevelDeletion(event) {
+    const isDeletionConfirmed = window.confirm('Delete this level?');
+    if (isDeletionConfirmed) {
+        removeLevelNode(event);
+        selectFirstLevelInRotation();
+        event.stopPropagation();
+    }
+}
+
+
+function highlightSelectedLevel(event) {
+    const levelRotation = document.getElementById('rotationContainer');
+    const selectedLevelElement = event.target;
+
+    for (const levelElement of levelRotation.children) {
+        const isSelectedLevelElement = levelElement === selectedLevelElement;
+        const classNameSelectedLevel = 'levelSelected';
+
+        if (isSelectedLevelElement) {
+            levelElement.classList.add(classNameSelectedLevel);
+        } else {
+            levelElement.classList.remove(classNameSelectedLevel);
+        }
+    }
+}
+
+
+function removeLevelNode(event) {
+    const deleteButton = event.currentTarget;
+    const levelElement = deleteButton.parentElement;
+    levelElement.remove();
+}
+
+
+function selectFirstLevelInRotation() {
+    const levelRotation = document.getElementById('rotationContainer');
+    const isLevelRotationEmpty = levelRotation.children.length === 0;
+
+    if (isLevelRotationEmpty) {
+        addLevel();
+    }
+    levelRotation.children[0].click();
 }
