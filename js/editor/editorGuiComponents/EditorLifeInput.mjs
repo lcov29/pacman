e30bf0fc8c5259097a3db92d8022e7f927588239
Lucas Vossen <lv_code@outlook.com>
2022-09-04 19:06:33 +0200

    add initialization of event listener and implement handling of same value input

--- js/editor/editorGuiComponents/EditorLifeInput.mjs
+++ js/editor/editorGuiComponents/EditorLifeInput.mjs
@@ -4,38 +4,70 @@ import Configuration from "../../global/Configuration.mjs";
 export default class EditorLifeInput {
 
 
+    #editor = null;
     #inputLife = null;
+    #previousLife = -1;
 
 
-    constructor(inputLifeId) {
-        this.#inputLife = document.getElementById(inputLifeId);
+    constructor(editorReference) {
+        this.#editor = editorReference;
+        this.#inputLife = document.getElementById('initialLifeInput');
     }
-    
+
 
     get life() {
-        return this.#inputLife.value;
+        return parseInt(this.#inputLife.value);
     }
 
 
     initialize() {
+        this.#initializeInputElement();
+        this.#addInputBlurEventListener();
+    }
+
+
+    updateLifeInputCallback() {
+        const isValid = this.#validate();
+        const isUpdated = this.#isLifeInputUpdated();
+
+        if (isValid && isUpdated) {
+            this.#updatePreviousLife();
+            this.#editor.handleLifeInputChange();
+        }
+    }
+
+
+    #initializeInputElement() {
         this.#inputLife.setAttribute('min', Configuration.editorMinLife);
         this.#inputLife.setAttribute('max', Configuration.editorMaxLife);
-        this.reset();
-    }
-
-
-    reset() {
         this.#inputLife.value = Configuration.editorDefaultLife;
+        this.#previousLife = Configuration.editorDefaultLife;
     }
 
 
-    validate() {
+    #addInputBlurEventListener() {
+        this.#inputLife.addEventListener('blur', this.updateLifeInputCallback.bind(this));
+    }
+
+
+    #validate() {
         const inputNumber = parseInt(this.#inputLife.value);
         const isNumber = !isNaN(inputNumber);
         const isInRange = (Configuration.editorMinLife <= inputNumber) && (inputNumber <= Configuration.editorMaxLife);
         const isValid =  isNumber && isInRange;
 
-        this.#inputLife.value = (isValid) ? inputNumber : Configuration.editorDefaultLife;
+        this.#inputLife.value = (isValid) ? inputNumber : this.#previousLife;
+        return isValid;
+    }
+
+
+    #isLifeInputUpdated() {
+        return parseInt(this.#inputLife.value) !==  this.#previousLife;
+    }
+
+
+    #updatePreviousLife() {
+        this.#previousLife = parseInt(this.#inputLife.value);
     }
 
 
