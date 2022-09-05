7d287555b0fa2a558814cf4f6c24bdf0aa3be132
Lucas Vossen <lv_code@outlook.com>
2022-09-05 05:45:45 +0200

    move initialization of tile click event listeners to new class EditorTileSelectionBar

--- js/editor/controller.js
+++ js/editor/controller.js
@@ -5,10 +5,3 @@ import Editor from './Editor.mjs';
 
 const editor = new Editor();
 editor.initialize();
-
-
-// add handlers to level elements in selector bar
-const radioList = document.querySelectorAll('input[name="selectors"]');
-for (const radio of radioList) {
-    radio.addEventListener('click', editor.handleSelectionTileClick.bind(editor));
-}
