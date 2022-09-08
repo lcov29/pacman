13eaedae62b9784643577de9281bacb86946a369
Lucas Vossen <lv_code@outlook.com>
2022-09-08 05:37:00 +0200

    add methods to display scatter and spawn position inputs

--- js/editor/editorGuiComponents/EditorScatterSpawnInput.mjs
+++ js/editor/editorGuiComponents/EditorScatterSpawnInput.mjs
@@ -8,6 +8,8 @@ export default class EditorScatterSpawnInput {
 
     #editor = null;
     #inputControlIdList = [];
+    #inputScatterControlIdList = [];
+    #inputSpawnControlIdList = [];
     #isGhostBlinkyScatterSpawnControlDisplayed = false;
     #isGhostPinkyScatterSpawnControlDisplayed = false;
     #isGhostClydeScatterSpawnControlDisplayed = false;
@@ -59,6 +61,29 @@ export default class EditorScatterSpawnInput {
     }
 
 
+    displayScatterPositionList(scatterPositionList) {
+        this.#displayScatterSpawnPositionList(scatterPositionList, this.#inputScatterControlIdList);
+    }
+
+
+    displaySpawnPositionList(spawnPositionList) {
+        this.#displayScatterSpawnPositionList(spawnPositionList, this.#inputSpawnControlIdList);
+    }
+
+
+    #displayScatterSpawnPositionList(positionList, inputControlIdList) {
+        for (const position of positionList) {
+            const ghostCharacter = position.ghost;
+            const controlIdObject = inputControlIdList.filter(element => element.ghostCharacter === ghostCharacter)[0];
+            const controlClassList = document.getElementById(controlIdObject.controlId).classList;
+            const inputPosition = document.getElementById(controlIdObject.inputId);
+          
+            controlClassList.remove('invisible');
+            inputPosition.value = `(${position.x},${position.y})`;
+        }
+    }
+
+
     reset() {
         this.#resetSpawnScatterInputElements();
         this.#resetSpawnScatterControlDisplayStatus();
@@ -85,16 +110,19 @@ export default class EditorScatterSpawnInput {
 
 
     #initializeInputControlIdList() {
-        this.#inputControlIdList = [
-            {inputId: 'scatterPositionGhostBlinky', controlId: 'scatterControlGhostBlinky'},
-            {inputId: 'scatterPositionGhostPinky', controlId: 'scatterControlGhostPinky'},
-            {inputId: 'scatterPositionGhostInky', controlId: 'scatterControlGhostInky'},
-            {inputId: 'scatterPositionGhostClyde', controlId: 'scatterControlGhostClyde'},
-            {inputId: 'spawnPositionGhostBlinky', controlId: 'spawnControlGhostBlinky'},
-            {inputId: 'spawnPositionGhostPinky', controlId: 'spawnControlGhostPinky'},
-            {inputId: 'spawnPositionGhostInky', controlId: 'spawnControlGhostInky'},
-            {inputId: 'spawnPositionGhostClyde', controlId: 'spawnControlGhostClyde'}
+        this.#inputScatterControlIdList = [
+            {ghostCharacter: Configuration.ghostBlinkyCharacter, inputId: 'scatterPositionGhostBlinky', controlId: 'scatterControlGhostBlinky'},
+            {ghostCharacter: Configuration.ghostPinkyCharacter, inputId: 'scatterPositionGhostPinky', controlId: 'scatterControlGhostPinky'},
+            {ghostCharacter: Configuration.ghostInkyCharacter, inputId: 'scatterPositionGhostInky', controlId: 'scatterControlGhostInky'},
+            {ghostCharacter: Configuration.ghostClydeCharacter, inputId: 'scatterPositionGhostClyde', controlId: 'scatterControlGhostClyde'}
         ];
+        this.#inputSpawnControlIdList = [
+            {ghostCharacter: Configuration.ghostBlinkyCharacter, inputId: 'spawnPositionGhostBlinky', controlId: 'spawnControlGhostBlinky'},
+            {ghostCharacter: Configuration.ghostPinkyCharacter, inputId: 'spawnPositionGhostPinky', controlId: 'spawnControlGhostPinky'},
+            {ghostCharacter: Configuration.ghostInkyCharacter, inputId: 'spawnPositionGhostInky', controlId: 'spawnControlGhostInky'},
+            {ghostCharacter: Configuration.ghostClydeCharacter, inputId: 'spawnPositionGhostClyde', controlId: 'spawnControlGhostClyde'}
+        ];
+        this.#inputControlIdList = [...this.#inputScatterControlIdList, ... this.#inputSpawnControlIdList]; 
     }
 
 
