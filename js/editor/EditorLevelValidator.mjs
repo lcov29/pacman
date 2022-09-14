a41f418cb6eac7bcc79bc12b1b12e12f6d7de1cc
Lucas Vossen <lv_code@outlook.com>
2022-09-14 04:32:30 +0200

    add validation for ghost scatter positions and add method getErrorMessageString()

--- js/editor/EditorLevelValidator.mjs
+++ js/editor/EditorLevelValidator.mjs
@@ -4,71 +4,117 @@ export default class EditorLevelValidator {
 
 
     #board = [[]];
+    #scatterPositionList = [];
     #validationErrorMessageList = [];
     #pacmanNumber = 0;
+    #ghostBlinkyNumber = 0;
+    #ghostPinkyNumber = 0;
+    #ghostInkyNumber = 0;
+    #ghostClydeNumber = 0;
     #pointPowerupNumber = 0;
     #teleporter1Number = 0;
     #teleporter2Number = 0;
     #teleporter3Number = 0;
 
 
-    constructor(board) {
+    constructor(board, scatterPositionList) {
         this.#board = board;
-    }
-
-
-    get errorMessageList() {
-        return this.#validationErrorMessageList;
+        this.#scatterPositionList = scatterPositionList;
     }
 
 
     validate() {
         this.#countLevelElements();
         this.#validatePacmanNumber();
+        this.#validateGhostBlinkyScatterPositionList();
+        this.#validateGhostPinkyScatterPositionList();
+        this.#validateGhostInkyScatterPositionList();
+        this.#validateGhostClydeScatterPositionList();
         this.#validatePointPowerupNumber();
         this.#validateTeleporter1Number();
         this.#validateTeleporter2Number();
         this.#validateTeleporter3Number();
-        return this.#isValid();
     }
 
 
-    #isValid() {
+    getErrorMessageString() {
+        const messageString = this.#validationErrorMessageList.join('\n');
+        return messageString;
+    }
+
+
+    isValid() {
         return this.#validationErrorMessageList.length === 0;
     }
 
 
     #validatePacmanNumber() {
         const isPacmanNumberValid = this.#pacmanNumber > 0;
-        const validationErrorMessage = 'Level requires at least one pacman';
+        const validationErrorMessage = '- Level requires at least one pacman';
         this.#validateCondition(isPacmanNumberValid, validationErrorMessage);
+    }
+
+
+    #validateGhostBlinkyScatterPositionList() {
+        const isGhostBlinkyOnBoard = this.#ghostBlinkyNumber > 0;
+        const hasScatterPosition = this.#scatterPositionList.filter(element => element.ghost === Configuration.ghostBlinkyCharacter).length > 0;
+        const isGhostBlinkyScatterPositionValid = !isGhostBlinkyOnBoard || (isGhostBlinkyOnBoard && hasScatterPosition);
+        const validationErrorMessage = '- Scatter position for ghost Blinky required';
+        this.#validateCondition(isGhostBlinkyScatterPositionValid, validationErrorMessage);
+    }
+
+
+    #validateGhostPinkyScatterPositionList() {
+        const isGhostPinkyOnBoard = this.#ghostPinkyNumber > 0;
+        const hasScatterPosition = this.#scatterPositionList.filter(element => element.ghost === Configuration.ghostPinkyCharacter).length > 0;
+        const isGhostPinkyScatterPositionValid = !isGhostPinkyOnBoard || (isGhostPinkyOnBoard && hasScatterPosition);
+        const validationErrorMessage = '- Scatter position for ghost Pinky required';
+        this.#validateCondition(isGhostPinkyScatterPositionValid, validationErrorMessage);
+    }
+
+
+    #validateGhostInkyScatterPositionList() {
+        const isGhostInkyOnBoard = this.#ghostInkyNumber > 0;
+        const hasScatterPosition = this.#scatterPositionList.filter(element => element.ghost === Configuration.ghostInkyCharacter).length > 0;
+        const isGhostInkyScatterPositionValid = !isGhostInkyOnBoard || (isGhostInkyOnBoard && hasScatterPosition);
+        const validationErrorMessage = '- Scatter position for ghost Inky required';
+        this.#validateCondition(isGhostInkyScatterPositionValid, validationErrorMessage);
+    }
+
+
+    #validateGhostClydeScatterPositionList() {
+        const isGhostClydeOnBoard = this.#ghostClydeNumber > 0;
+        const hasScatterPosition = this.#scatterPositionList.filter(element => element.ghost === Configuration.ghostClydeCharacter).length > 0;
+        const isGhostClydeScatterPositionValid = !isGhostClydeOnBoard || (isGhostClydeOnBoard && hasScatterPosition);
+        const validationErrorMessage = '- Scatter position for ghost Clyde required';
+        this.#validateCondition(isGhostClydeScatterPositionValid, validationErrorMessage);
     }
 
 
     #validatePointPowerupNumber() {
         const isPointOrPowerupNumberValid = this.#pointPowerupNumber > 0;
-        const validationErrorMessage = 'Level requires at least one point or powerup';
+        const validationErrorMessage = '- Level requires at least one point or powerup';
         this.#validateCondition(isPointOrPowerupNumberValid, validationErrorMessage);
     }
 
 
     #validateTeleporter1Number() {
         const isTeleporter1NumberValid = (this.#teleporter1Number === 0) || (this.#teleporter1Number === 2);
-        const validationErrorMessage = 'Level requires either zero or two tiles of teleporter1 (red)';
+        const validationErrorMessage = '- Level requires either zero or two tiles of teleporter1 (red)';
         this.#validateCondition(isTeleporter1NumberValid, validationErrorMessage);
     }
 
 
     #validateTeleporter2Number() {
         const isTeleporter2NumberValid = (this.#teleporter2Number === 0) || (this.#teleporter2Number === 2);
-        const validationErrorMessage = 'Level requires either zero or two tiles of teleporter1 (red)';
+        const validationErrorMessage = '- Level requires either zero or two tiles of teleporter1 (green)';
         this.#validateCondition(isTeleporter2NumberValid, validationErrorMessage);
     }
 
 
     #validateTeleporter3Number() {
         const isTeleporter3NumberValid = (this.#teleporter3Number === 0) || (this.#teleporter3Number === 2);
-        const validationErrorMessage = 'Level requires either zero or two tiles of teleporter1 (red)';
+        const validationErrorMessage = '- Level requires either zero or two tiles of teleporter1 (blue)';
         this.#validateCondition(isTeleporter3NumberValid, validationErrorMessage);
     }
 
@@ -88,6 +134,30 @@ export default class EditorLevelValidator {
                 const isPacmanCharacter = currentCharacter === Configuration.pacmanCharacter;
                 if (isPacmanCharacter) {
                     this.#pacmanNumber++;
+                    continue;
+                }
+
+                const isGhostBlinkyCharacter = currentCharacter === Configuration.ghostBlinkyCharacter;
+                if (isGhostBlinkyCharacter) {
+                    this.#ghostBlinkyNumber++;
+                    continue;
+                }
+
+                const isGhostPinkyCharacter = currentCharacter === Configuration.ghostPinkyCharacter;
+                if (isGhostPinkyCharacter) {
+                    this.#ghostPinkyNumber++;
+                    continue;
+                }
+
+                const isGhostInkyCharacter = currentCharacter === Configuration.ghostInkyCharacter;
+                if (isGhostInkyCharacter) {
+                    this.#ghostInkyNumber++;
+                    continue;
+                }
+
+                const isGhostClydeCharacter = currentCharacter === Configuration.ghostClydeCharacter;
+                if (isGhostClydeCharacter) {
+                    this.#ghostClydeNumber++;
                     continue;
                 }
 
